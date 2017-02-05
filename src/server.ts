f97e6ac1fdea60f1e12120d5bbbf03973083d620
kamil.mysliwiec <kamil.mysliwiec@frogriot.com>
2017-02-05 22:55:09 +0100

    Travis config update

--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import { NestRunner } from "./nest/runner";
-import { Application } from "./app/app";
-import { ApplicationModule } from "./app/modules/app.module";
-
-NestRunner.run<Application>(Application, ApplicationModule);
