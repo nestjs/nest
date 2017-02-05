7dcb91cfaab1e31879f1a4c24e4edfd1bc80ee2d
kamil.mysliwiec <kamil.mysliwiec@frogriot.com>
2017-02-05 23:50:25 +0100

    Travis integration

--- example/app.ts
+++ example/app.ts
@@ -5,9 +5,9 @@ export class Application implements NestApplication {
     constructor(private app) {}
 
     start() {
-        this.app.listen(3030, () => {
+        const server = this.app.listen(3030, () => {
             console.log("Application listen on port:", 3030);
-            this.app.close();
+            server.close();
         });
     }
  
