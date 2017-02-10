bb8dc1ef745f706cc7a0087383aa1b6584bbe3fe
kamil.mysliwiec <kamil.mysliwiec@frogriot.com>
2017-02-10 19:47:30 +0100

    Remove directories

--- tests.webpack.js
+++
@@ -1,7 +0,0 @@
-var chai = require('chai');
-var chaiSinon = require('sinon-chai');
-
-chai.use(chaiSinon);
-
-var context = require.context('./src/', true, /.spec\.[jt]sx?$/);
-context.keys().forEach(context);
