5447a5c4a198f2d6cc19948df76a45b9e5ef81a3
kamil.mysliwiec <kamil.mysliwiec@frogriot.com>
2017-03-25 20:56:07 +0100

    Fix exports

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -3,4 +3,5 @@ export * from './controller.decorator';
 export * from './component.decorator';
 export * from './module.decorator';
 export * from './dependencies.decorator';
+export * from './inject.decorator';
 export { Component as Middleware } from './component.decorator';
