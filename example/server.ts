becd8c34454c431674bd41a413d8f4cb658d3fa4
kamil.mysliwiec <kamil.mysliwiec@frogriot.com>
2017-05-23 17:31:21 +0200

    Travis CI update

--- example/server.ts
+++ example/server.ts
@@ -8,8 +8,9 @@ const microservice = app.connectMicroservice({
     transport: Transport.TCP,
     port: 5667,
 });
-microservice.listen(() => ({}));
+
+app.startAllMicroservices(() => console.log('All microservices are listening...'));
 app.listen(port, () => {
     console.log('Application listen on port:', port);
-    //app.close();
+    app.close();
 });
