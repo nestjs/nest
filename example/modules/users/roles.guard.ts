4095915d339f92b4f18b39f9807ce94a929049f7
kamil.mysliwiec <kamil.mysliwiec@frogriot.com>
2017-07-23 12:49:41 +0200

    remove unecessary directiories

--- example/modules/users/roles.guard.ts
+++
@@ -1,8 +0,0 @@
-import { Guard, CanActivate } from '@nestjs/common';
-
-@Guard()
-export class RolesGuard implements CanActivate {
-    public async canActivate() {
-        return await Promise.resolve(true);
-    }
-}
