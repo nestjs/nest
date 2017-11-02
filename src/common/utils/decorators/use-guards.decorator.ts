95c2d98318d135d94c9d4a24453daafa4cc5b521
Nicolas <nicolas.lagoutte@connected-ventures.com>
2017-11-02 14:09:49 +0000

    'setuped' => 'set up'

--- src/common/utils/decorators/use-guards.decorator.ts
+++ src/common/utils/decorators/use-guards.decorator.ts
@@ -3,10 +3,10 @@ import { GUARDS_METADATA } from '../../constants';
 /**
  * Setups guards to the chosen context.
  * When the `@UseGuards()` is used on the controller level:
- * - Guard will be setuped to the every handler (every method)
+ * - Guard will be set up to every handler (every method)
  *
  * When the `@UseGuards()` is used on the handle level:
- * - Guard will be setuped only to specified method
+ * - Guard will be set up only to specified method
  *
  * @param  {} ...guards (types)
  */
