c86d5a4844340abbcbb93b6ab3b84b2a9e8c7ad4
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2017-11-02 20:14:21 +0100

    Remove body-parser from examples

--- examples/01-cats-app/src/server.ts
+++ examples/01-cats-app/src/server.ts
@@ -1,14 +1,10 @@
-import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 import { ValidationPipe } from './modules/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-
-  app.use(bodyParser.json());
   app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(3000);
 }
 bootstrap();
