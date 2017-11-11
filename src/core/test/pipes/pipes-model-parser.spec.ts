339a33582c89ee21e2ac3c080063897fc1066e22
Adam <adam.michael.frank@gmail.com>
2017-11-11 07:25:59 -0500

    Improved test coverage

--- src/core/test/pipes/pipes-model-parser.spec.ts
+++ src/core/test/pipes/pipes-model-parser.spec.ts
@@ -2,14 +2,25 @@ import * as sinon from "sinon";
 import { expect } from "chai";
 import { PipesModelParser } from "./../../pipes/pipes-model-parser";
 import { ArgumentMetadata } from "@nestjs/common";
+import { IsString } from "class-validator";
 
 class TestModel {
-  constructor(public prop1: string, public prop2: number) {}
+  constructor() {}
+  @IsString()
+  public prop1: string;
+
+  @IsString()
+  public prop2: string;
 }
 
 describe("PipesModelParser", () => {
   let target: PipesModelParser;
   let testModel;
+  const metadata: ArgumentMetadata = {
+    type: "body",
+    metatype: TestModel,
+    data: ""
+  };
   beforeEach(() => {
     target = new PipesModelParser();
   });
@@ -24,15 +35,16 @@ describe("PipesModelParser", () => {
       });
     });
     describe("when metadata contains a class", () => {
-      const metadata: ArgumentMetadata = {
-        type: "body",
-        metatype: TestModel,
-        data: ""
-      };
       it("should return an instance of the class", async () => {
         const testObj = { prop1: "value1", prop2: "value2" };
         const result = await target.transform(testObj, metadata);
         expect(result).to.be.instanceOf(TestModel);
+      });
+    });
+    describe("when validation vails", () => {
+      it("should throw an error", async () => {
+        const testObj = { prop1: "value1" };
+        return expect(target.transform(testObj, metadata)).to.be.rejected;
       });
     });
   });
