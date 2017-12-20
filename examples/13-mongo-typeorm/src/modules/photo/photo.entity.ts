b605b8871eb0df2141036b380d357a8f24345c88
Hector Chauran <hector_gonzalez_2005@hotmail.com>
2017-12-20 19:22:08 -0400

    Removed unused import
    `PrimaryGeneratedColumn` is not  being used in `photo.entity.ts` because the example uses mongodb.

--- examples/13-mongo-typeorm/src/modules/photo/photo.entity.ts
+++ examples/13-mongo-typeorm/src/modules/photo/photo.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, ObjectIdColumn, ObjectID } from 'typeorm';
+import { Entity, Column, ObjectIdColumn, ObjectID } from 'typeorm';
 
 @Entity()
 export class Photo {
