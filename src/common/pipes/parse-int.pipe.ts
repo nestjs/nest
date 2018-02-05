e089a4730f0be4b467f62288a8d1cd1bf5b17d67
Oli1810 <olivier.sch68@gmail.com>
2018-02-05 12:40:08 +0100

    feat(@nestjs/common): update validation condition
    This commit also removes the unused rxjs import

--- src/common/pipes/parse-int.pipe.ts
+++ src/common/pipes/parse-int.pipe.ts
@@ -1,12 +1,11 @@
-import { BadRequestException } from '../exceptions/bad-request.exception';
-import { PipeTransform } from '../interfaces/pipe-transform.interface';
-import { Pipe, ArgumentMetadata } from '../index';
-import {isNumeric} from "rxjs/util/isNumeric";
+import {BadRequestException} from '../exceptions/bad-request.exception';
+import {PipeTransform} from '../interfaces/pipe-transform.interface';
+import {ArgumentMetadata, Pipe} from '../index';
 
 @Pipe()
 export class ParseIntPipe implements PipeTransform<string> {
   async transform(value: string, metadata: ArgumentMetadata): Promise<number> {
-    if ('string' === typeof value && isNumeric(value)) {
+    if ('string' === typeof value && !isNaN(parseFloat(value)) && isFinite(value as any)) {
      return parseInt(value, 10);
     }
     else {
