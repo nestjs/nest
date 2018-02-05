ec6b89160db0450404d2712886e2de59a542738a
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-02-05 16:14:12 +0100

    feature(@nestjs/common) refactor ParseIntPipe

--- src/common/pipes/parse-int.pipe.ts
+++ src/common/pipes/parse-int.pipe.ts
@@ -1,15 +1,17 @@
-import {BadRequestException} from '../exceptions/bad-request.exception';
-import {PipeTransform} from '../interfaces/pipe-transform.interface';
-import {ArgumentMetadata, Pipe} from '../index';
+import { BadRequestException } from '../exceptions/bad-request.exception';
+import { PipeTransform } from '../interfaces/pipe-transform.interface';
+import { ArgumentMetadata, Pipe } from '../index';
 
 @Pipe()
 export class ParseIntPipe implements PipeTransform<string> {
   async transform(value: string, metadata: ArgumentMetadata): Promise<number> {
-    if ('string' === typeof value && !isNaN(parseFloat(value)) && isFinite(value as any)) {
-     return parseInt(value, 10);
-    }
-    else {
+    const isNumeric =
+      'string' === typeof value &&
+      !isNaN(parseFloat(value)) &&
+      isFinite(value as any);
+    if (!isNumeric) {
       throw new BadRequestException('Numeric string is expected');
     }
+    return parseInt(value, 10);
   }
 }
