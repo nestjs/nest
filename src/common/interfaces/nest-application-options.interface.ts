deaf3a2dcbba38461f56c0ee43b415c56b596dec
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-03-01 22:26:52 +0100

    feature(@nestjs/core) make cors middleware customizable

--- src/common/interfaces/nest-application-options.interface.ts
+++ src/common/interfaces/nest-application-options.interface.ts
@@ -1,9 +1,10 @@
 import { HttpsOptions } from './https-options.interface';
 import { LoggerService } from '../services/logger.service';
 import { NestApplicationContextOptions } from './nest-application-context-options.interface';
+import { CorsOptions } from './external/cors-options.interface';
 
 export interface NestApplicationOptions extends NestApplicationContextOptions {
-  cors?: boolean;
+  cors?: boolean | CorsOptions;
   bodyParser?: boolean;
   httpsOptions?: HttpsOptions;
 }
