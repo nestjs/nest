531b2fe018cb9c384e70aa512923eb31533d8b23
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-03-09 15:59:26 +0100

    update(@nestjs) upgrade reflect-metadata package

--- examples/01-cats-app/src/main.ts
+++ examples/01-cats-app/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 import { ValidationPipe } from './common/pipes/validation.pipe';
+import { FastifyAdapter } from '@nestjs/core/adapters/fastify-adapter';
+import * as fastify from 'fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
+  const app = await NestFactory.create(ApplicationModule, new FastifyAdapter(fastify()));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
