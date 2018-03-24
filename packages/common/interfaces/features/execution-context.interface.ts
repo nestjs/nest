0eb3354344159e88d877f5260aeacc74e7241d90
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-03-24 15:28:40 +0100

    /version-5: Auto stash before merge of "update/version-5" and "origin/update/version-5"

--- packages/common/interfaces/features/execution-context.interface.ts
+++ packages/common/interfaces/features/execution-context.interface.ts
@@ -1,4 +1,8 @@
+import { Type } from './../index';
+
 export interface ExecutionContext {
-  parent: Function;
-  handler: (...args) => any;
+  getClass<T = any>(): Type<T>;
+  getHandler(): Function;
+  getArgs<T extends Array<any> = any[]>(): T;
+  getArgByIndex<T = any>(index: number): T;
 }
