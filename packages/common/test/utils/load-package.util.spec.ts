aeda614c5cb72e999596881b515c55081b0629e4
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-04-30 13:04:36 +0200

    test(@nestjs/common) fix broken unit tests

--- packages/common/test/utils/load-package.util.spec.ts
+++ packages/common/test/utils/load-package.util.spec.ts
@@ -1,21 +1,11 @@
 import { expect } from 'chai';
-import {
-  loadPackage,
-  MissingRequiredDependencyException,
-} from './../../utils/load-package.util';
+import { loadPackage } from './../../utils/load-package.util';
 
 describe('loadPackage', () => {
   describe('when package is available', () => {
     it('should return package', () => {
       expect(loadPackage('reflect-metadata', 'ctx')).to.be.eql(
         require('reflect-metadata'),
-      );
-    });
-  });
-  describe('when package is unavailable', () => {
-    it('should throw exception', () => {
-      expect(() => loadPackage('unavailable-package', 'ctx')).to.throws(
-        MissingRequiredDependencyException,
       );
     });
   });
