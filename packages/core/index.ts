72aad1d90ea2963e963deb87378385d0972c25cf
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-05-06 22:41:06 +0200

    bugfix(@nestjs/core) export FastifyAdapter at the root level #622

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -9,6 +9,7 @@ export { MiddlewareBuilder } from './middleware/builder';
 export { ModuleRef } from './injector/module-ref';
 export { NestFactory } from './nest-factory';
 export { APP_INTERCEPTOR, APP_FILTER, APP_GUARD, APP_PIPE } from './constants';
+export * from './adapters';
 export * from './services';
 export * from './nest-application';
 export * from './nest-application-context';
