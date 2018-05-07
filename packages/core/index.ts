c5e560d1cf7a1ed788bf55e3028550d22983c977
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-05-07 13:18:48 +0200

    samples(@nestjs) fix grpc client options, remove slash (swagger)

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -8,6 +8,7 @@
 export { MiddlewareBuilder } from './middleware/builder';
 export { ModuleRef } from './injector/module-ref';
 export { NestFactory } from './nest-factory';
+export { HTTP_SERVER_REF } from './injector/tokens';
 export { APP_INTERCEPTOR, APP_FILTER, APP_GUARD, APP_PIPE } from './constants';
 export * from './adapters';
 export * from './services';
