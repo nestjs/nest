1fb1a41ee990e46b3cabae4976d723a8cb36803b
Omkar Yadav <httpsomkar@gmail.com>
2018-05-21 12:47:12 +0530

    Added option for the logging in Fixes #709

--- packages/testing/test.ts
+++ packages/testing/test.ts
@@ -9,16 +9,17 @@ import { Logger } from '@nestjs/common/services/logger.service';
 import { NestEnvironment } from '@nestjs/common/enums/nest-environment.enum';
 import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 import { TestingModuleBuilder } from './testing-module.builder';
+import { TestOptions } from './interfaces/test-options.interface';
 
 export class Test {
   private static readonly metadataScanner = new MetadataScanner();
 
-  public static createTestingModule(metadata: ModuleMetadata) {
-    this.init();
+  public static createTestingModule(metadata: ModuleMetadata, testOptions: TestOptions) {
+    this.init(testOptions);
     return new TestingModuleBuilder(this.metadataScanner, metadata);
   }
 
-  private static init() {
-    Logger.setMode(NestEnvironment.TEST);
+  private static init(testOptions: TestOptions) {
+    Logger.setMode(testOptions.logging);
   }
 }
