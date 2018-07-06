357b60372946958686b388f080f31605d85d1c90
tan90 <tan90indream@163.com>
2018-07-06 11:33:26 +0800

    refactor(@nestjs/websocket) ws support for connection req
    add a second arg of `handleConnection` to support  (client, req) => {} callback of the `connection` event

--- packages/websockets/interfaces/nest-gateway.interface.ts
+++ packages/websockets/interfaces/nest-gateway.interface.ts
@@ -1,5 +1,5 @@
 export interface NestGateway {
   afterInit?: (server: any) => void;
-  handleConnection?: (client: any) => void;
+  handleConnection?: (client: any, ...rest: any[]) => void;
   handleDisconnect?: (client: any) => void;
 }
