014a7c92afb284e6f6ef386f1ab967ebf17aabc8
Anton <antonlarichev@gmail.com>
2018-07-26 17:43:27 +0300

    feature(@nestjs/microservices) Integration tests

---
+++ integration/microservices/e2e/broadcast-rmq.spec.ts
@@ -0,0 +1,49 @@
+import * as express from 'express';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { RMQController } from '../src/rmq/rmq.controller';
+import { RMQBroadcastController } from '../src/rmq/rmq-broadcast.controller';
+
+describe('RabbitMQ transport', () => {
+  let server;
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [RMQBroadcastController],
+    }).compile();
+
+    server = express();
+    app = module.createNestApplication(server);
+    app.connectMicroservice({
+      transport: Transport.RMQ,
+      options: {
+        url: `amqp://admin:admin@localhost`,
+        queue: 'test',
+        queueOptions: { durable: false }
+      }
+    });
+    app.connectMicroservice({
+      transport: Transport.RMQ,
+      options: {
+        url: `amqp://admin:admin@localhost`,
+        queue: 'test',
+        queueOptions: { durable: false }
+      }
+    });
+    await app.startAllMicroservicesAsync();
+    await app.init();
+  });
+
+  it(`Broadcast (2 subscribers)`, () => {
+    return request(server)
+      .get('/broadcast')
+      .expect(200, '2');
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
---
+++ integration/microservices/src/rmq/rmq-broadcast.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  Client,
+  MessagePattern,
+  ClientProxy,
+  Transport,
+  ClientProxyFactory
+} from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { scan, take } from 'rxjs/operators';
+
+@Controller()
+export class RMQBroadcastController {
+  client: ClientProxy;
+
+  constructor() {
+    this.client = ClientProxyFactory.create({
+        transport: Transport.RMQ,
+        options: {
+          url: `amqp://admin:admin@localhost`,
+          queue: 'test',
+          queueOptions: { durable: false }
+        }
+      });
+  }
+
+  @Get('broadcast')
+  multicats() {
+    return this.client
+      .send<number>({ cmd: 'broadcast' }, {})
+      .pipe(scan((a, b) => a + b), take(2));
+  }
+
+  @MessagePattern({ cmd: 'broadcast' })
+  replyBroadcast(): Observable<number> {
+    return new Observable(observer => observer.next(1));
+  }
+}
