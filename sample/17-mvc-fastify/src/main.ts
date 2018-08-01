2ea58acc70cf4d81ac8ffb47e30c24105666c47b
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-08-01 22:38:47 +0200

    sample(@nestjs) fix paths join usage

--- sample/17-mvc-fastify/src/main.ts
+++ sample/17-mvc-fastify/src/main.ts
@@ -5,14 +5,14 @@ import { ApplicationModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule, new FastifyAdapter());
   app.useStaticAssets({
-    root: join(__dirname, './../public'),
+    root: join(__dirname, '..', 'public'),
     prefix: '/public/',
   });
   app.setViewEngine({
     engine: {
       handlebars: require('handlebars'),
     },
-    templates: join(__dirname, './../views'),
+    templates: join(__dirname, '..', 'views'),
   });
   await app.listen(3000);
 }
