76beabe686a1b9a0770ea2a2e98bb591ad263453
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-09-05 10:07:50 +0200

    sample(nestjs) update sample http cache interceptor

--- sample/12-graphql-apollo/src/app.module.ts
+++ sample/12-graphql-apollo/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
@@ -8,6 +9,10 @@ import { CatsModule } from './cats/cats.module';
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       installSubscriptionHandlers: true,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.schema.d.ts'),
+        outputAs: 'class',
+      },
     }),
   ],
 })
