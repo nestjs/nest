03f2832a2d9667b8ccb66e849db8b61a569ae68a
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-09-13 10:20:24 +0200

    sample(graphql) update graphql sample (add validation)

--- sample/12-graphql-apollo/src/app.module.ts
+++ sample/12-graphql-apollo/src/app.module.ts
@@ -10,7 +10,7 @@ import { CatsModule } from './cats/cats.module';
       typePaths: ['./**/*.graphql'],
       installSubscriptionHandlers: true,
       definitions: {
-        path: join(process.cwd(), 'src/graphql.schema.d.ts'),
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
         outputAs: 'class',
       },
     }),
---
+++ sample/12-graphql-apollo/src/graphql.schema.ts
@@ -0,0 +1,26 @@
+export class CreateCatInput {
+    name?: string;
+    age?: number;
+}
+
+export class Cat {
+    id?: number;
+    name?: string;
+    age?: number;
+}
+
+export abstract class IMutation {
+    abstract createCat(createCatInput?: CreateCatInput): Cat | Promise<Cat>;
+}
+
+export abstract class IQuery {
+    abstract getCats(): Cat[] | Promise<Cat[]>;
+
+    abstract cat(id: string): Cat | Promise<Cat>;
+
+    abstract temp__(): boolean | Promise<boolean>;
+}
+
+export abstract class ISubscription {
+    abstract catCreated(): Cat | Promise<Cat>;
+}
