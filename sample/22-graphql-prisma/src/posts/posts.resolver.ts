ff828b9e863004b4141ab04b3c8374fda494b61e
Christian Schröter <dev@cschroeter.net>
2018-09-14 15:09:35 +0200

    feat: Implement all supoorted queries and mutations

--- sample/22-graphql-prisma/src/posts/posts.resolver.ts
+++ sample/22-graphql-prisma/src/posts/posts.resolver.ts
@@ -7,6 +7,7 @@ import {
   Info,
 } from '@nestjs/graphql';
 import { PrismaService } from '../prisma/prisma.service';
+import { BatchPayload } from '../prisma/prisma.binding';
 import { Post } from '../graphql.schema';
 
 @Resolver()
@@ -26,6 +27,26 @@ export class PostsResolver {
   @Mutation('createPost')
   async createPost(@Args() args, @Info() info): Promise<Post> {
     return await this.prisma.mutation.createPost(args, info);
+  }
+
+  @Mutation('updatePost')
+  async updatePost(@Args() args, @Info() info): Promise<Post> {
+    return await this.prisma.mutation.updatePost(args, info);
+  }
+
+  @Mutation('updateManyPosts')
+  async updateManyPosts(@Args() args, @Info() info): Promise<BatchPayload> {
+    return await this.prisma.mutation.updateManyPosts(args, info);
+  }
+
+  @Mutation('deletePost')
+  async deletePost(@Args() args, @Info() info): Promise<Post> {
+    return await this.prisma.mutation.deletePost(args, info);
+  }
+
+  @Mutation('deleteManyPosts')
+  async deleteManyPosts(@Args() args, @Info() info): Promise<BatchPayload> {
+    return await this.prisma.mutation.deleteManyPosts(args, info);
   }
 
   @Subscription('post')
