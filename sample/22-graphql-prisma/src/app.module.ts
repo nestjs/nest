447372a485289ef44024305234bee884dabcf78c
Christian Schröter <dev@cschroeter.net>
2018-09-14 15:10:37 +0200

    refactor: Extract GraphQL related configuration in a dedicated GraphqlOptions class

--- sample/22-graphql-prisma/src/app.module.ts
+++ sample/22-graphql-prisma/src/app.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostsModule } from './posts/posts.module';
+import { GraphqlOptions } from 'graphql.options';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql'],
-      path: '/',
-      installSubscriptionHandlers: true,
-      resolverValidationOptions: {
-        requireResolversForResolveType: false,
-      },
-      definitions: {
-        path: join(process.cwd(), 'src/graphql.schema.d.ts'),
-        outputAs: 'class',
-      },
+    GraphQLModule.forRootAsync({
+      useClass: GraphqlOptions,
     }),
     PrismaModule,
     PostsModule,
