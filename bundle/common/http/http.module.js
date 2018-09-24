26eeeb598e28803127e8c9618374701dbb4e5da6
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-09-24 15:14:09 +0200

    chore(nestjs) publish 5.3.8 release

--- bundle/common/http/http.module.js
+++ bundle/common/http/http.module.js
@@ -8,25 +8,35 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
 const module_decorator_1 = require("../decorators/modules/module.decorator");
+const random_string_generator_util_1 = require("../utils/random-string-generator.util");
 const http_constants_1 = require("./http.constants");
 const http_service_1 = require("./http.service");
 let HttpModule = HttpModule_1 = class HttpModule {
     static register(config) {
         return {
             module: HttpModule_1,
-            providers: [{
+            providers: [
+                {
                     provide: http_constants_1.AXIOS_INSTANCE_TOKEN,
                     useValue: axios_1.default.create(config),
-                }],
+                },
+                {
+                    provide: http_constants_1.HTTP_MODULE_ID,
+                    useValue: random_string_generator_util_1.randomStringGenerator(),
+                },
+            ],
         };
     }
 };
 HttpModule = HttpModule_1 = __decorate([
     module_decorator_1.Module({
-        providers: [http_service_1.HttpService, {
+        providers: [
+            http_service_1.HttpService,
+            {
                 provide: http_constants_1.AXIOS_INSTANCE_TOKEN,
                 useValue: axios_1.default,
-            }],
+            },
+        ],
         exports: [http_service_1.HttpService],
     })
 ], HttpModule);
