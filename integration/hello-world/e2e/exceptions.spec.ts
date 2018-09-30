f45021e7cb396712923406e3c0366eafc0b599e5
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2018-09-30 21:08:44 +0200

    refactor(integration) resolve module path

--- integration/hello-world/e2e/exceptions.spec.ts
+++ integration/hello-world/e2e/exceptions.spec.ts
@@ -1,7 +1,6 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication, HttpStatus } from '@nestjs/common';
-import { ApplicationModule } from './../src/app.module';
 import { ErrorsController } from '../src/errors/errors.controller';
 
 describe('Error messages', () => {
--- integration/hello-world/e2e/hello-world.spec.ts
+++ integration/hello-world/e2e/hello-world.spec.ts
@@ -1,7 +1,7 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { ApplicationModule } from './../src/app.module';
+import { ApplicationModule } from '../src/app.module';
 
 describe('Hello world (default adapter)', () => {
   let server;
