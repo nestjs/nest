a6b001638fd252b094b043cd5a42957b905c6f80
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-10-17 21:42:24 +0200

    refactor: extract handle message from WsAdapter

--- packages/websockets/adapters/ws-adapter.ts
+++ packages/websockets/adapters/ws-adapter.ts
@@ -15,10 +15,10 @@ import { MessageMappingProperties } from '../gateway-metadata-explorer';
 let wsPackage: any = {};
 
 enum READY_STATE {
-    CONNECTING_STATE = 0,
-    OPEN_STATE = 1,
-    CLOSING_STATE = 2,
-    CLOSED_STATE = 3,
+  CONNECTING_STATE = 0,
+  OPEN_STATE = 1,
+  CLOSING_STATE = 2,
+  CLOSED_STATE = 3,
 }
 
 export class WsAdapter implements WebSocketAdapter {
@@ -79,11 +79,13 @@ export class WsAdapter implements WebSocketAdapter {
       ),
       takeUntil(close$),
     );
-    source$.subscribe(response => {
-      if (client.readyState === READY_STATE.OPEN_STATE) {
-          client.send(JSON.stringify(response));
+    const handleMessage = response => {
+      if (client.readyState !== READY_STATE.OPEN_STATE) {
+        return;
       }
-    });
+      client.send(JSON.stringify(response));
+    };
+    source$.subscribe(handleMessage);
   }
 
   public bindMessageHandler(
@@ -108,7 +110,9 @@ export class WsAdapter implements WebSocketAdapter {
   }
 
   public bindErrorHandler(server) {
-    server.on(CONNECTION_EVENT, ws => ws.on(ERROR_EVENT, err => this.logger.error(err)));
+    server.on(CONNECTION_EVENT, ws =>
+      ws.on(ERROR_EVENT, err => this.logger.error(err)),
+    );
     server.on(ERROR_EVENT, err => this.logger.error(err));
     return server;
   }
