b49a9a109858b0b4365497bc1ebec82ab119157f
Fiona Lovett <fiona@beiq.com>
2019-01-02 17:53:46 +0100

    feature(common): add transformOptions to ValidationPipeOptions
    allows plainToClass to expose class properties to defined groups as per issue #1374

--- packages/common/pipes/validation.pipe.ts
+++ packages/common/pipes/validation.pipe.ts
@@ -6,6 +6,7 @@ import {
   ValidationError,
 } from '../index';
 import { ValidatorOptions } from '../interfaces/external/validator-options.interface';
+import { ClassTransformOptions } from '../interfaces/external/class-transform-options.interface';
 import { PipeTransform } from '../interfaces/features/pipe-transform.interface';
 import { loadPackage } from '../utils/load-package.util';
 import { isNil } from '../utils/shared.utils';
@@ -13,6 +14,7 @@ import { isNil } from '../utils/shared.utils';
 export interface ValidationPipeOptions extends ValidatorOptions {
   transform?: boolean;
   disableErrorMessages?: boolean;
+  transformOptions?: ClassTransformOptions;
   exceptionFactory?: (errors: ValidationError[]) => any;
 }
 
@@ -24,13 +26,20 @@ export class ValidationPipe implements PipeTransform<any> {
   protected isTransformEnabled: boolean;
   protected isDetailedOutputDisabled?: boolean;
   protected validatorOptions: ValidatorOptions;
+  protected transformOptions: ClassTransformOptions;
   protected exceptionFactory: (errors: ValidationError[]) => any;
 
   constructor(@Optional() options?: ValidationPipeOptions) {
     options = options || {};
-    const { transform, disableErrorMessages, ...validatorOptions } = options;
+    const {
+      transform,
+      disableErrorMessages,
+      transformOptions,
+      ...validatorOptions
+    } = options;
     this.isTransformEnabled = !!transform;
     this.validatorOptions = validatorOptions;
+    this.transformOptions = transformOptions;
     this.isDetailedOutputDisabled = disableErrorMessages;
     this.exceptionFactory =
       options.exceptionFactory ||
@@ -52,6 +61,7 @@ export class ValidationPipe implements PipeTransform<any> {
     const entity = classTransformer.plainToClass(
       metatype,
       this.toEmptyIfNil(value),
+      this.transformOptions,
     );
     const errors = await classValidator.validate(entity, this.validatorOptions);
     if (errors.length > 0) {
@@ -60,8 +70,8 @@ export class ValidationPipe implements PipeTransform<any> {
     return this.isTransformEnabled
       ? entity
       : Object.keys(this.validatorOptions).length > 0
-      ? classTransformer.classToPlain(entity)
-      : value;
+        ? classTransformer.classToPlain(entity, this.transformOptions)
+        : value;
   }
 
   private toValidate(metadata: ArgumentMetadata): boolean {
