68be7e250496f8e76024967f1cd62b8417bdf3ea
EreckGordon <ereckgordon@gmail.com>
2019-01-04 15:33:23 -0700

    re-add NestGateway to exports
    NestGateway was removed from exports (mistakenly?) when hooks were moved to their own folder. Re adds so the interface may be imported.

--- packages/websockets/interfaces/index.ts
+++ packages/websockets/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './hooks';
 export * from './observable-socket-server.interface';
 export * from './web-socket-server.interface';
 export * from './ws-response.interface';
+export * from './nest-gateway.interface';
