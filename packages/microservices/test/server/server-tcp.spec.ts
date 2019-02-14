72b84ec97cd7adb715c17c1c5faa60e67198e1f6
Igor Savin <igor.savin@adidas.com>
2019-02-14 17:29:53 +0100

    build(sinon) fix more TypeScript compilation errors

--- packages/microservices/test/server/server-tcp.spec.ts
+++ packages/microservices/test/server/server-tcp.spec.ts
@@ -14,7 +14,7 @@ describe('ServerTCP', () => {
     const socket = { on: sinon.spy() };
     beforeEach(() => {
       getSocketInstance = sinon
-        .stub(server, 'getSocketInstance')
+        .stub(server, 'getSocketInstance' as any)
         .callsFake(() => socket);
     });
     it('should bind message event to handler', () => {
