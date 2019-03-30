2fa9989072098f9319adc237df22a26d69e7b867
Jiayi Hu <jiayi.ghu@gmail.com>
2019-03-30 21:45:24 +0100

    fix(microservices): export ClientRMQ and ServerRMQ

--- packages/microservices/server/index.ts
+++ packages/microservices/server/index.ts
@@ -3,4 +3,5 @@ export * from './server-grpc';
 export * from './server-mqtt';
 export * from './server-nats';
 export * from './server-redis';
+export * from './server-rmq';
 export * from './server-tcp';
