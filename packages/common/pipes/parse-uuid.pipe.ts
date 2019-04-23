3319e62d627663cbf1014593d87bdd59d5ab6f80
Gustavo Perdomo <gperdomor@gmail.com>
2019-04-23 13:10:02 -0400

    uuid pipes

---
+++ packages/common/pipes/parse-uuid.pipe.ts
@@ -0,0 +1,22 @@
+import isUUID from 'validator/lib/isUUID';
+import { Optional } from '../decorators';
+import { BadRequestException } from '../exceptions/bad-request.exception';
+import { ArgumentMetadata, Injectable } from '../index';
+import { PipeTransform } from '../interfaces/features/pipe-transform.interface';
+
+@Injectable()
+export class ParseUUIDPipe implements PipeTransform<string> {
+  private readonly version: '3' | '4' | '5';
+
+  constructor(@Optional() version?: '3' | '4' | '5') {
+    this.version = version;
+  }
+  async transform(value: string, metadata: ArgumentMetadata): Promise<string> {
+    if (!isUUID(value, this.version)) {
+      throw new BadRequestException(
+        `Validation failed (uuid v${this.version} is expected)`,
+      );
+    }
+    return value;
+  }
+}
