03293bf77b520b07a4e2f0305bb94e9374126071
Anton Repin <anton.repin@alation.com>
2019-04-25 18:46:53 -0700

    Client metadata can be provided as a Promise object which will require unwrap before it will be read by Nest starter

--- packages/microservices/decorators/client.decorator.ts
+++ packages/microservices/decorators/client.decorator.ts
@@ -6,8 +6,28 @@ import { ClientOptions } from '../interfaces/client-metadata.interface';
  *
  * @param  {ClientOptions} metadata optional client metadata
  */
-export const Client = (metadata?: ClientOptions) => {
+export const Client = (metadata?: ClientOptions | Promise<ClientOptions>) => {
   return (target: object, propertyKey: string | symbol): void => {
+    /*
+     *  Check if metadata will require unwrapping before using
+     */
+    // @ts-ignore
+    if (metadata instanceof Promise || typeof metadata.then === 'function') {
+      (metadata as Promise<ClientOptions>).then(m => {
+        Reflect.set(target, propertyKey, null);
+        Reflect.defineMetadata(CLIENT_METADATA, true, target, propertyKey);
+        Reflect.defineMetadata(
+          CLIENT_CONFIGURATION_METADATA,
+          m,
+          target,
+          propertyKey,
+        );
+      });
+      return;
+    }
+    /*
+     *  Just assign metadata to Reflector without unwrapping it
+     */
     Reflect.set(target, propertyKey, null);
     Reflect.defineMetadata(CLIENT_METADATA, true, target, propertyKey);
     Reflect.defineMetadata(
