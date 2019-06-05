999da7655d9385c3090c3e38da1860afbe1d7a8c
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-06-05 09:47:40 +0200

    refactor(common): simplify isUUID helper function

--- packages/common/utils/is-uuid.ts
+++ packages/common/utils/is-uuid.ts
@@ -1,4 +1,4 @@
-import { assertString } from './assert-string';
+import { BadRequestException } from '../exceptions';
 
 const uuid = {
   3: /^[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
@@ -8,7 +8,9 @@ const uuid = {
 };
 
 export function isUUID(str: any, version = 'all') {
-  assertString(str);
+  if (typeof str !== 'string') {
+    throw new BadRequestException('The value passed as UUID is not a string');
+  }
   const pattern = uuid[version];
   return pattern && pattern.test(str);
 }
