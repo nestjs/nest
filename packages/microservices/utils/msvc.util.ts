5ddd8cb74e663021a196106141b45c68f34ffbbd
Andrey <geass1000@gmail.com>
2019-06-21 13:35:48 +0300

    style(microservices): change indentation to space (2)

--- packages/microservices/utils/msvc.util.ts
+++ packages/microservices/utils/msvc.util.ts
@@ -2,38 +2,38 @@ import { isString, isObject } from '@nestjs/common/utils/shared.utils';
 
 export class MsvcUtil {
 
-    /**
-     * Transforms the Pattern to Route.
-     * 1. If Pattern is a `string`, it will be returned as it is.
-     * 2. If Pattern is a `JSON` object, it will be transformed to Route. For that end,
-     * the function will sort properties of `JSON` Object and creates `route` string
-     * according to the following template:
-     * <key1>:<value1>/<key2>:<value2>/.../<keyN>:<valueN>
-     *
-     * @param  {any} pattern - client pattern
-     * @returns string
-     */
-    public static transformPatternToRoute(pattern: any): string {
-        // Returns the pattern according to the 1st
-        if (isString(pattern)) {
-            return pattern;
-        }
-
-        // Throws the error if the pattern has an incorrect type
-        if (!isObject(pattern)) {
-            throw new Error(`The pattern must be of type 'string' or 'object'!`);
-        }
-
-        // Gets keys of the JSON Pattern and sorts them
-        const sortedKeys = Object.keys(pattern)
-            .sort((a, b) => ('' + a).localeCompare(b));
-
-        // Creates the array of Pattern params from sorted keys and their corresponding values
-        const sortedPatternParams = sortedKeys.map((key) =>
-            `${key}:${MsvcUtil.transformPatternToRoute(pattern[key])}`);
-
-        // Creates and returns the Route
-        const route = sortedPatternParams.join('/');
-        return `{${route}}`;
+  /**
+   * Transforms the Pattern to Route.
+   * 1. If Pattern is a `string`, it will be returned as it is.
+   * 2. If Pattern is a `JSON` object, it will be transformed to Route. For that end,
+   * the function will sort properties of `JSON` Object and creates `route` string
+   * according to the following template:
+   * <key1>:<value1>/<key2>:<value2>/.../<keyN>:<valueN>
+   *
+   * @param  {any} pattern - client pattern
+   * @returns string
+   */
+  public static transformPatternToRoute(pattern: any): string {
+    // Returns the pattern according to the 1st
+    if (isString(pattern)) {
+      return pattern;
     }
+
+    // Throws the error if the pattern has an incorrect type
+    if (!isObject(pattern)) {
+      throw new Error(`The pattern must be of type 'string' or 'object'!`);
+    }
+
+    // Gets keys of the JSON Pattern and sorts them
+    const sortedKeys = Object.keys(pattern)
+      .sort((a, b) => ('' + a).localeCompare(b));
+
+    // Creates the array of Pattern params from sorted keys and their corresponding values
+    const sortedPatternParams = sortedKeys.map((key) =>
+      `${key}:${MsvcUtil.transformPatternToRoute(pattern[key])}`);
+
+    // Creates and returns the Route
+    const route = sortedPatternParams.join('/');
+    return `{${route}}`;
+  }
 }
