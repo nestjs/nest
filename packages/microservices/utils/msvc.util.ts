2faca6634e8d49b58f5d19d57848c84f5e5d03df
Andrey <geass1000@gmail.com>
2019-06-24 10:49:54 +0300

    feature(microservices): add handling of fields with `number` type

--- packages/microservices/utils/msvc.util.ts
+++ packages/microservices/utils/msvc.util.ts
@@ -1,11 +1,13 @@
 import { isString, isObject } from '@nestjs/common/utils/shared.utils';
+import * as Interfaces from '../interfaces';
 
 export class MsvcUtil {
 
   /**
    * Transforms the Pattern to Route.
    * 1. If Pattern is a `string`, it will be returned as it is.
-   * 2. If Pattern is a `JSON` object, it will be transformed to Route. For that end,
+   * 2. If Pattern is a `number`, it will be converted to `string`.
+   * 3. If Pattern is a `JSON` object, it will be transformed to Route. For that end,
    * the function will sort properties of `JSON` Object and creates `route` string
    * according to the following template:
    * <key1>:<value1>/<key2>:<value2>/.../<keyN>:<valueN>
@@ -14,9 +16,9 @@ export class MsvcUtil {
    * @returns string
    */
   public static transformPatternToRoute(pattern: Interfaces.MsPattern): string {
-    // Returns the pattern according to the 1st
-    if (isString(pattern)) {
-      return pattern;
+    // Returns the pattern according to the 1st and the 2nd points
+    if (isString(pattern) || typeof pattern === 'number') {
+      return `${pattern}`;
     }
 
     // Throws the error if the pattern has an incorrect type
