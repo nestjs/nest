9bd91f3fd8fd9a52a3dd0daea75a8ee43b3480e3
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-07-08 14:12:35 +0200

    feature(grpc): modify rpc exception filter so it forward Grpc exception

--- packages/microservices/exceptions/base-rpc-exception-filter.ts
+++ packages/microservices/exceptions/base-rpc-exception-filter.ts
@@ -3,6 +3,8 @@ import { isObject, isError } from '@nestjs/common/utils/shared.utils';
 import { MESSAGES } from '@nestjs/core/constants';
 import { Observable, throwError as _throw } from 'rxjs';
 import { RpcException } from './rpc-exception';
+import { GrpcException } from './grpc-exceptions';
+import { GrpcStatus } from '../enums/grpc-status.enum';
 
 export class BaseRpcExceptionFilter<T = any, R = any>
   implements RpcExceptionFilter<T> {
@@ -10,19 +12,25 @@ export class BaseRpcExceptionFilter<T = any, R = any>
 
   catch(exception: T, host: ArgumentsHost): Observable<R> {
     const status = 'error';
-    if (!(exception instanceof RpcException)) {
-      const errorMessage = MESSAGES.UNKNOWN_EXCEPTION_MESSAGE;
-
-      const loggerArgs = isError(exception)
-        ? [exception.message, exception.stack]
-        : [exception];
-      const logger = BaseRpcExceptionFilter.logger;
-      logger.error.apply(logger, loggerArgs as any);
-
-      return _throw({ status, message: errorMessage });
+    if (exception instanceof RpcException) {
+      const res = exception.getError();
+      const message = isObject(res) ? res : { status, message: res };
+      return _throw(message);
     }
-    const res = exception.getError();
-    const message = isObject(res) ? res : { status, message: res };
-    return _throw(message);
+    if (exception instanceof GrpcException) {
+      const code = exception.getCode();
+      const error = exception.getError();
+      const message = isObject(error) ? error : { code, message: error };
+      return _throw(message);
+    }
+    const errorMessage = MESSAGES.UNKNOWN_EXCEPTION_MESSAGE;
+
+    const loggerArgs = isError(exception)
+      ? [exception.message, exception.stack]
+      : [exception];
+    const logger = BaseRpcExceptionFilter.logger;
+    logger.error.apply(logger, loggerArgs as any);
+
+    return _throw({ status, message: errorMessage });
   }
 }
