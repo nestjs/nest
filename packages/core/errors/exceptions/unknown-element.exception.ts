f1271b6f1ef97209f81a0b2a8a36a2fe7385b0a9
Andrei Miulescu <andreimc@users.noreply.github.com>
2019-08-12 12:29:53 +1000

    Update packages/core/errors/exceptions/unknown-element.exception.ts
    Co-Authored-By: Livio Brunner <livio.brunner.lb1@gmail.com>

--- packages/core/errors/exceptions/unknown-element.exception.ts
+++ packages/core/errors/exceptions/unknown-element.exception.ts
@@ -3,7 +3,7 @@ import { RuntimeException } from './runtime.exception';
 export class UnknownElementException extends RuntimeException {
   constructor(name?: string) {
     super(
-      `Nest cannot find ${name ? name : 'given'} element (it does not exist in current context)`,
+      `Nest cannot find ${name || 'given'} element (it does not exist in current context)`,
     );
   }
 }
