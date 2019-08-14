a90de69cc26cf9795f05395ff70a69ce6bb4925f
Ben Wildeman <ben.wildeman@connectchildcare.com>
2019-08-14 15:37:14 +0100

    refactor: Add MulterFile interface

--- packages/platform-express/multer/interfaces/multer-options.interface.ts
+++ packages/platform-express/multer/interfaces/multer-options.interface.ts
@@ -1,3 +1,24 @@
+export interface MulterFile {
+  /** Field name specified in the form */
+  fieldname: string;
+  /** Name of the file on the user's computer */
+  originalname: string;
+  /** Encoding type of the file */
+  encoding: string;
+  /** Mime type of the file */
+  mimetype: string;
+  /** Size of the file in bytes */
+  size: number;
+  /** The folder to which the file has been saved (DiskStorage) */
+  destination: string;
+  /** The name of the file within the destination (DiskStorage) */
+  filename: string;
+  /** Location of the uploaded file (DiskStorage) */
+  path: string;
+  /** A Buffer of the entire file (MemoryStorage) */
+  buffer: Buffer;
+}
+
 /**
  * @see https://github.com/expressjs/multer
  */
@@ -29,26 +50,7 @@ export interface MulterOptions {
   };
   fileFilter?(
     req: any,
-    file: {
-      /** Field name specified in the form */
-      fieldname: string;
-      /** Name of the file on the user's computer */
-      originalname: string;
-      /** Encoding type of the file */
-      encoding: string;
-      /** Mime type of the file */
-      mimetype: string;
-      /** Size of the file in bytes */
-      size: number;
-      /** The folder to which the file has been saved (DiskStorage) */
-      destination: string;
-      /** The name of the file within the destination (DiskStorage) */
-      filename: string;
-      /** Location of the uploaded file (DiskStorage) */
-      path: string;
-      /** A Buffer of the entire file (MemoryStorage) */
-      buffer: Buffer;
-    },
+    file: MulterFile,
     callback: (error: Error | null, acceptFile: boolean) => void,
   ): void;
 }
