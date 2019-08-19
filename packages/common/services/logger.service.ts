9d394f56685ad210df507f11c19c393446873620
Rubin Bhandari <xidharthbhandari@gmail.com>
2019-08-19 12:52:14 +0545

    Added seconds to logger timestamp
    At the moment only hours and minutes are logged in the default Logger implementation.
    
    This pull request add seconds to default logger format.
    
    Note that I don't want to override the whole logger service. It's pretty nice, I just want another timestamp format

--- packages/common/services/logger.service.ts
+++ packages/common/services/logger.service.ts
@@ -111,6 +111,7 @@ export class Logger implements LoggerService {
       year: 'numeric',
       hour: 'numeric',
       minute: 'numeric',
+      second: 'numeric',
       day: '2-digit',
       month: '2-digit',
     };
