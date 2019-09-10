70c3f2e4767702b83c41e318629fc5c634ce2956
John Biundo <johnfbiundo@gmail.com>
2019-09-10 08:43:19 -0700

    fix code to match docs

--- sample/25-dynamic-modules/src/config/config.service.ts
+++ sample/25-dynamic-modules/src/config/config.service.ts
@@ -12,10 +12,7 @@ export class ConfigService {
   private readonly envConfig: EnvConfig;
 
   constructor(@Inject(CONFIG_OPTIONS) private options: ConfigOptions) {
-    const filePath = `${
-      process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
-    }.env`;
-
+    const filePath = `${process.env.NODE_ENV || 'development'}.env`;
     const envFile = path.resolve(__dirname, '../../', options.folder, filePath);
     this.envConfig = dotenv.parse(fs.readFileSync(envFile));
   }
