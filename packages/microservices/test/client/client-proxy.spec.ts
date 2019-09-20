00425084cb408d0f669001882411576fc51570a3
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-20 22:34:04 +0200

    test(microservices) add stub fake call to dispatchEvent

--- packages/microservices/test/client/client-proxy.spec.ts
+++ packages/microservices/test/client/client-proxy.spec.ts
@@ -160,7 +160,9 @@ describe('ClientProxy', function() {
       it(`should call "dispatchEvent"`, () => {
         const pattern = { test: 3 };
         const data = 'test';
-        const dispatchEventSpy = sinon.spy();
+        const dispatchEventSpy = sinon
+          .stub()
+          .callsFake(() => Promise.resolve(true));
         const stream$ = client.emit(pattern, data);
         client['dispatchEvent'] = dispatchEventSpy;
 
