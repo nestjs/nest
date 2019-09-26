28badda2cd51151e2c25c3661da9ecf300408f2d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-26 15:19:46 +0200

    feat(core) add more utility reflector methods

--- packages/core/test/services/reflector.service.spec.ts
+++ packages/core/test/services/reflector.service.spec.ts
@@ -15,12 +15,34 @@ describe('Reflector', () => {
       expect(reflector.get(key, Test)).to.eql(value);
     });
   });
+
   describe('getAll', () => {
     it('should reflect metadata of all targets', () => {
       const key = 'key';
       const value = 'value';
       Reflect.defineMetadata(key, value, Test);
       expect(reflector.getAll(key, [Test])).to.eql([value]);
+    });
+  });
+
+  describe('getAllAndConcat', () => {
+    it('should reflect metadata of all targets and concat arrays', () => {
+      const key = 'key';
+      const value = 'value';
+      Reflect.defineMetadata(key, value, Test);
+      expect(reflector.getAllAndConcat(key, [Test, Test])).to.eql([
+        value,
+        value,
+      ]);
+    });
+  });
+
+  describe('getAllAndOverride', () => {
+    it('should reflect metadata of all targets and return a first not undefined value', () => {
+      const key = 'key';
+      const value = 'value';
+      Reflect.defineMetadata(key, value, Test);
+      expect(reflector.getAllAndOverride(key, [Test, Test])).to.eql(value);
     });
   });
 });
