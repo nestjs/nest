708663aff0f69b1039fbb636fbd414fc0b74db7c
John Biundo <johnfbiundo@gmail.com>
2019-10-04 16:19:28 -0700

    merge & wip 2

--- sample/15-mvc/src/main.ts
+++ sample/15-mvc/src/main.ts
@@ -4,9 +4,7 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
