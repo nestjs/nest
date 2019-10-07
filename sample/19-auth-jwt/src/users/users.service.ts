72b8eb98f5d3cf3575219f5624323a06ce0ea421
Mohammad-Quanit <muhammadquanit@gmail.com>
2019-10-07 15:15:32 +0500

    docs(changelog) update return type await in authentication/documentation

--- sample/19-auth-jwt/src/users/users.service.ts
+++ sample/19-auth-jwt/src/users/users.service.ts
@@ -27,6 +27,6 @@ export class UsersService {
   }
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+    return await this.users.find(user => user.username === username);
   }
 }
