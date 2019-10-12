faa8690465322fb288a4401fd2362ca87de66a1e
Kim Kern <github@kimkern.de>
2019-10-12 22:31:37 +0200

    doc(samples) add unit tests for 13-mongo-typeorm
    see #1539

---
+++ sample/13-mongo-typeorm/src/photo/photo.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { PhotoService } from './photo.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Photo } from './photo.entity';
+
+describe('Photo Service', () => {
+  let service: PhotoService;
+  let repository;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [PhotoService],
+      providers: [
+        {
+          provide: getRepositoryToken(Photo),
+          useFactory: jest.fn(() => ({
+            find: jest.fn(),
+          })),
+        },
+      ],
+    }).compile();
+    service = module.get<PhotoService>(PhotoService);
+    repository = module.get(getRepositoryToken(Photo));
+  });
+
+  it('should return all photos from the repository', async () => {
+    repository.find.mockReturnValueOnce([]);
+    expect(await service.findAll()).toEqual([]);
+    expect(repository.find).toHaveBeenCalledTimes(1);
+  });
+
+});
