4605515a606ce62546cf2a30f0e4bd9e409a34be
Kim Kern <github@kimkern.de>
2019-10-12 22:31:56 +0200

    doc(samples) add e2e test for 13-mongo-typeorm
    see #1539

---
+++ sample/13-mongo-typeorm/test/photos.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Photos (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
+  it('should return an empty list of photos', async () => {
+    await request(app.getHttpServer())
+      .get('/photo')
+      .expect(200)
+      .expect(res => expect(res.body)
+        .toEqual([]));
+  });
+});
