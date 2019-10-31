658c42247ebf78f8e8efc9bc19de702b2a808612
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-10-31 13:40:26 +0100

    fix(microservices): fix noack option in rmq client

--- packages/microservices/server/server-rmq.ts
+++ packages/microservices/server/server-rmq.ts
@@ -85,8 +85,7 @@ export class ServerRMQ extends Server implements CustomTransportStrategy {
   }
 
   public async setupChannel(channel: any, callback: Function) {
-    const noAck =
-      this.getOptionsProp(this.options, 'noAck', RQM_DEFAULT_NOACK);
+    const noAck = this.getOptionsProp(this.options, 'noAck', RQM_DEFAULT_NOACK);
 
     await channel.assertQueue(this.queue, this.queueOptions);
     await channel.prefetch(this.prefetchCount, this.isGlobalPrefetchCount);
