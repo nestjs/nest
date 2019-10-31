ddcee872bfa798f4051d003f7eaf4f347511e91e
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2019-10-31 13:16:57 +0100

    Update cache-ttl.decorator.ts

--- packages/common/cache/decorators/cache-ttl.decorator.ts
+++ packages/common/cache/decorators/cache-ttl.decorator.ts
@@ -2,11 +2,9 @@ import { SetMetadata } from '../../decorators';
 import { CACHE_TTL_METADATA } from '../cache.constants';
 
 /**
- * Decorator that sets the cache ttl setting the duration for cache expiration for
- * Web sockets or Microservice based apps.
+ * Decorator that sets the cache ttl setting the duration for cache expiration.
  *
- * For example:
- * `@CacheTTL(5)`
+ * For example: `@CacheTTL(5)`
  *
  * @param ttl number set the cache expiration time
  *
