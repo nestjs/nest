83717cec5c7ba08c9db15d3dd770a670ce80e721
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-11-03 21:06:24 +0100

    fix(decorators): allow using custom decorators with rcp & ws #3318

--- packages/websockets/constants.ts
+++ packages/websockets/constants.ts
@@ -1,3 +1,5 @@
+import { ROUTE_ARGS_METADATA } from '@nestjs/common/constants';
+
 export const MESSAGE_MAPPING_METADATA = 'websockets:message_mapping';
 export const MESSAGE_METADATA = 'message';
 export const GATEWAY_SERVER_METADATA = 'websockets:is_socket';
@@ -5,7 +7,7 @@ export const GATEWAY_METADATA = 'websockets:is_gateway';
 export const NAMESPACE_METADATA = 'namespace';
 export const PORT_METADATA = 'port';
 export const GATEWAY_OPTIONS = 'websockets:gateway_options';
-export const PARAM_ARGS_METADATA = 'websockets:args';
+export const PARAM_ARGS_METADATA = ROUTE_ARGS_METADATA;
 
 export const CONNECTION_EVENT = 'connection';
 export const DISCONNECT_EVENT = 'disconnect';
