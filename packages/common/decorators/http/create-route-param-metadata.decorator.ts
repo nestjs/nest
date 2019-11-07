ec9f9132533ead4f832d9b84569818c20a7945ae
Vasil Rangelov <vasil.tech@condor-gaming.com>
2019-11-07 17:51:32 +0200

    feat(decorators) Added pipe overrides and types.
    Added a new optional pipeList parameter, that allows decorators to opt out of the request's pipes, filter them, or override them with custom ones.
    
    Also added generic types to the createParamDecorator function, which are passed down to the factory, allowing the caller to make the factory type safe.

--- packages/common/decorators/http/create-route-param-metadata.decorator.ts
+++ packages/common/decorators/http/create-route-param-metadata.decorator.ts
@@ -5,7 +5,7 @@ import {
 } from '../../constants';
 import { PipeTransform } from '../../index';
 import { Type } from '../../interfaces';
-import { CustomParamFactory } from '../../interfaces/features/custom-route-param-factory.interface';
+import { CustomParamFactory } from '../../interfaces';
 import { isFunction, isNil } from '../../utils/shared.utils';
 import { ParamData, RouteParamsMetadata } from './route-params.decorator';
 
@@ -32,10 +32,20 @@ export type ParamDecoratorEnhancer = ParameterDecorator;
  * Defines HTTP route param decorator
  *
  * @param factory
+ * @param enhancers
+ * @param pipeList A callback that accepts a list of all pipes that would be
+ *     applied to the result, and returns
+ *     a list of pipes to actually apply to the result.
+ *     If not specified, all would be pipes are actually applied.
  */
-export function createParamDecorator(
-  factory: CustomParamFactory,
+export function createParamDecorator<
+  TFactoryData = any,
+  TFactoryRequest = any,
+  TFactoryResult = any
+>(
+  factory: CustomParamFactory<TFactoryData, TFactoryRequest, TFactoryResult>,
   enhancers: ParamDecoratorEnhancer[] = [],
+  pipeList?: (pipes?: (Type<PipeTransform> | PipeTransform)[]) => (Type<PipeTransform> | PipeTransform)[],
 ): (
   ...dataOrPipes: (Type<PipeTransform> | PipeTransform | any)[]
 ) => ParameterDecorator {
@@ -56,7 +66,9 @@ export function createParamDecorator(
 
     const hasParamData = isNil(data) || !isPipe(data);
     const paramData = hasParamData ? data : undefined;
-    const paramPipes = hasParamData ? pipes : [data, ...pipes];
+    const pipesToApply =
+      typeof pipeList !== 'undefined' ? pipeList(pipes) : pipes;
+    const paramPipes = hasParamData ? pipesToApply : [data, ...pipesToApply];
 
     Reflect.defineMetadata(
       ROUTE_ARGS_METADATA,
