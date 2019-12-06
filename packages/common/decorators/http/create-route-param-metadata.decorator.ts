0b89142e4384e79c86f7baf94ea77e412cb46496
galesky <leonardo.galesky@gmail.com>
2019-12-06 00:27:22 -0300

    feat(common): typed decorators through generics

--- packages/common/decorators/http/create-route-param-metadata.decorator.ts
+++ packages/common/decorators/http/create-route-param-metadata.decorator.ts
@@ -27,7 +27,13 @@ const assignCustomMetadata = (
 });
 
 export type ParamDecoratorEnhancer = ParameterDecorator;
-
+type Unpacked<T> = T extends (infer U)[]
+  ? U
+  : T extends (...args: any[]) => infer U
+  ? U
+  : T extends Promise<infer U>
+  ? U
+  : T;
 /**
  * Defines HTTP route param decorator
  *
@@ -43,6 +49,48 @@ export function createParamDecorator(
   return (
     data?,
     ...pipes: (Type<PipeTransform> | PipeTransform)[]
+  ): ParameterDecorator => (target, key, index) => {
+    const args =
+      Reflect.getMetadata(ROUTE_ARGS_METADATA, target.constructor, key) || {};
+
+    const isPipe = (pipe: any) =>
+      pipe &&
+      ((isFunction(pipe) &&
+        pipe.prototype &&
+        isFunction(pipe.prototype.transform)) ||
+        isFunction(pipe.transform));
+
+    const hasParamData = isNil(data) || !isPipe(data);
+    const paramData = hasParamData ? data : undefined;
+    const paramPipes = hasParamData ? pipes : [data, ...pipes];
+
+    Reflect.defineMetadata(
+      ROUTE_ARGS_METADATA,
+      assignCustomMetadata(
+        args,
+        paramtype,
+        index,
+        factory,
+        paramData,
+        ...(paramPipes as PipeTransform[]),
+      ),
+      target.constructor,
+      key,
+    );
+    enhancers.forEach(fn => fn(target, key, index));
+  };
+}
+
+export function createGenericParamDecorator<T>(
+  factory: CustomParamFactory,
+  enhancers: ParamDecoratorEnhancer[] = [],
+): (
+  ...dataOrPipes: (Type<PipeTransform> | PipeTransform | Unpacked<T>)[]
+) => ParameterDecorator {
+  const paramtype = uuid();
+  return (
+    data?,
+    ...pipes: (Type<PipeTransform> | PipeTransform | Unpacked<T>)[]
   ): ParameterDecorator => (target, key, index) => {
     const args =
       Reflect.getMetadata(ROUTE_ARGS_METADATA, target.constructor, key) || {};
