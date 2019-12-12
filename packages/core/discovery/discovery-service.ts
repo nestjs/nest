b1af335ebd9e810c11c9e13efa523af1a3db7b79
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-12-12 19:37:26 +0100

    feat(core): add discovery module

---
+++ packages/core/discovery/discovery-service.ts
@@ -0,0 +1,44 @@
+import { flatten, Injectable } from '@nestjs/common';
+import { Module } from '../injector/module';
+import { ModulesContainer } from '../injector/modules-container';
+
+export interface DiscoveryOptions {
+  include?: Function[];
+}
+
+@Injectable()
+export class DiscoveryService {
+  constructor(private readonly modulesContainer: ModulesContainer) {}
+
+  getProviders(
+    options: DiscoveryOptions = {},
+    modules: Module[] = this.getModules(options),
+  ) {
+    const providers = modules.map(item => [...item.providers.values()]);
+    return flatten(providers);
+  }
+
+  getControllers(
+    options: DiscoveryOptions = {},
+    modules: Module[] = this.getModules(options),
+  ) {
+    const controllers = modules.map(item => [...item.controllers.values()]);
+    return flatten(controllers);
+  }
+
+  protected getModules(options: DiscoveryOptions = {}): Module[] {
+    if (!options.include) {
+      const moduleRefs = [...this.modulesContainer.values()];
+      return moduleRefs;
+    }
+    const whitelisted = this.includeWhitelisted(options.include);
+    return whitelisted;
+  }
+
+  private includeWhitelisted(include: Function[]): Module[] {
+    const moduleRefs = [...this.modulesContainer.values()];
+    return moduleRefs.filter(({ metatype }) =>
+      include.some(item => item === metatype),
+    );
+  }
+}
