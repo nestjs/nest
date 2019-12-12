ca7be04e40372436389cf19be0215f4656d3ebc7
Sahar Biton <sahar.bit@gmail.com>
2019-12-12 13:33:28 +0200

    Update main.ts to use redis-io adapter

--- sample/02-gateways/src/main.ts
+++ sample/02-gateways/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
