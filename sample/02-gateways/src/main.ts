e79c5c6a0d3de6e4ac5f0fd08b1e2c54d0ad7922
DEO <f.db@163.com>
2020-01-23 11:35:50 +0800

    Add missing adapter for redis.io

--- sample/02-gateways/src/main.ts
+++ sample/02-gateways/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
