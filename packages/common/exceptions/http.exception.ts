221a103ecdfd3fd122039aada6a1a01922debb5f
Saulo <saulotoledo@gmail.com>
2020-02-11 23:26:47 +0100

    refactor(common): fix exceptions doc/var names
    Fix incorrect documentation strings for HTTP exceptions
    and a few variable names.
    
    Closes #4042

--- packages/common/exceptions/http.exception.ts
+++ packages/common/exceptions/http.exception.ts
@@ -16,23 +16,23 @@ export class HttpException extends Error {
    * `throw new HttpException()`
    *
    * @usageNotes
-   * The constructor arguments define the HTTP response.
+   * The constructor arguments define the response and the HTTP response status code.
    * - The `response` argument (required) defines the JSON response body.
    * - The `status` argument (required) defines the HTTP Status Code.
    *
    * By default, the JSON response body contains two properties:
-   * - `statusCode`: defaults to the Http Status Code provided in the `error` argument
+   * - `statusCode`: the Http Status Code.
    * - `message`: a short description of the HTTP error by default; override this
    * by supplying a string in the `response` parameter.
    *
-   * To override the entire JSON response body, pass an object.  Nest will serialize
-   * the object and return it as the JSON response body.
+   * To override the entire JSON response body, pass an object to the `createBody`
+   * method. Nest will serialize the object and return it as the JSON response body.
    *
    * The `status` argument is required, and should be a valid HTTP status code.
    * Best practice is to use the `HttpStatus` enum imported from `nestjs/common`.
    *
    * @param response string or object describing the error condition.
-   * @param status HTTP response status code
+   * @param status HTTP response status code.
    */
   constructor(
     private readonly response: string | Record<string, any>,
