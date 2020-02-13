101e387ca8b26ffa2e75298aed1c6ecda0c0c673
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-02-13 10:10:27 +0100

    docs(): add public api annotations

--- packages/core/discovery/discovery-module.ts
+++ packages/core/discovery/discovery-module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { MetadataScanner } from '../metadata-scanner';
 import { DiscoveryService } from './discovery-service';
 
+/**
+ * @publicApi
+ */
 @Module({
   providers: [MetadataScanner, DiscoveryService],
   exports: [MetadataScanner, DiscoveryService],
