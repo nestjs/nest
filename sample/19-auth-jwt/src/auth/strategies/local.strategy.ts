05fb2393e4477d293afac5f45048910edd07bece
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-02-19 11:27:45 +0100

    sample(): update the auth jwt sample

--- sample/19-auth-jwt/src/auth/strategies/local.strategy.ts
+++ sample/19-auth-jwt/src/auth/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
-import { Strategy } from 'passport-local';
-import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
