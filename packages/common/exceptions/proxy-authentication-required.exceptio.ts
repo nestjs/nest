5ed53da319346cdf64b9d8383154cef2eb24dd13
Yasser Ameur el idrissi <37160072+getspooky@users.noreply.github.com>
2020-03-01 23:41:57 +0100

    feat(common): add Proxy Authentication Required Exception

---
+++ packages/common/exceptions/proxy-authentication-required.exceptio.ts
@@ -0,0 +1,50 @@
+import { HttpException } from './http.exception';
+import { HttpStatus } from '../enums/http-status.enum';
+
+/**
+ * Defines an HTTP exception for *Proxy Authentication Required* type errors.
+ *
+ * @see [Base Exceptions](https://docs.nestjs.com/exception-filters#base-exceptions)
+ *
+ * @publicApi
+ */
+export class ProxyAuthenticationRequiredException extends HttpException {
+  /**
+   * Instantiate a `ProxyAuthenticationRequiredException` Exception.
+   *
+   * @example
+   * `throw new HttpVersionNotSupportedException()`
+   *
+   * @usageNotes
+   * The constructor arguments define the HTTP response.
+   * - The `message` argument defines the JSON response body.
+   * - The `error` argument defines the HTTP Status Code.
+   *
+   * By default, the JSON response body contains two properties:
+   * - `statusCode`: defaults to the Http Status Code provided in the `error` argument
+   * - `message`: the string `'Proxy Authentication Required'` by default; override this by supplying
+   * a string in the `message` parameter.
+   *
+   * To override the entire JSON response body, pass an object.  Nest will serialize
+   * the object and return it as the JSON response body.
+   *
+   * The `error` argument is required, and should be a valid HTTP status code.
+   * Best practice is to use the `HttpStatus` enum imported from `nestjs/common`.
+   *
+   * @param message string or object describing the error condition.
+   * @param error HTTP response status code
+   */
+  constructor(
+    message?: string | object | any,
+    error = 'Proxy Authentication Required',
+  ) {
+    super(
+      HttpException.createBody(
+        message,
+        error,
+        HttpStatus.PROXY_AUTHENTICATION_REQUIRED,
+      ),
+      HttpStatus.PROXY_AUTHENTICATION_REQUIRED,
+    );
+  }
+}
