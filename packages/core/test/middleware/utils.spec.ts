f0c62802bdb0a8ba89096aaec407b665b515ce28
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-03-02 14:11:29 +0100

    fix(): fix unit tests and nyc configuration

--- packages/core/test/middleware/utils.spec.ts
+++ packages/core/test/middleware/utils.spec.ts
@@ -1,6 +1,6 @@
 import { RequestMethod, Type } from '@nestjs/common';
 import { expect } from 'chai';
-import { pathToRegexp } from 'path-to-regexp';
+import * as pathToRegexp from 'path-to-regexp';
 import * as sinon from 'sinon';
 import {
   assignToken,
