005c2c4360302d9d147bedcb83c917be11f9e98f
Ziv Bar <zivbr@hotmail.com>
2020-04-09 10:57:37 +0300

    fixed catsProvider's useFactory param type

--- sample/14-mongoose-base/src/cats/cats.providers.ts
+++ sample/14-mongoose-base/src/cats/cats.providers.ts
@@ -1,10 +1,10 @@
-import { Connection } from 'mongoose';
+import { Mongoose } from 'mongoose';
 import { CatSchema } from './schemas/cat.schema';
 
 export const catsProviders = [
   {
     provide: 'CAT_MODEL',
-    useFactory: (connection: Connection) => connection.model('Cat', CatSchema),
+    useFactory: (mongoose: Mongoose) => mongoose.model('Cat', CatSchema),
     inject: ['DATABASE_CONNECTION'],
   },
 ];
