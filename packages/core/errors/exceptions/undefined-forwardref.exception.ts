1bc99390007d0edcb290617d10afcf8d3047a8ff
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2020-05-12 12:08:10 +0200

    Update packages/core/errors/exceptions/undefined-forwardref.exception.ts

--- packages/core/errors/exceptions/undefined-forwardref.exception.ts
+++ packages/core/errors/exceptions/undefined-forwardref.exception.ts
@@ -2,7 +2,7 @@ import { UNDEFINED_FORWARDREF_MESSAGE } from '../messages';
 import { RuntimeException } from './runtime.exception';
 import { Type } from '@nestjs/common';
 
-export class UndefinedForwardrefException extends RuntimeException {
+export class UndefinedForwardRefException extends RuntimeException {
   constructor(scope: Type<any>[]) {
     super(UNDEFINED_FORWARDREF_MESSAGE(scope));
   }
