ae08b121192bc20ea4d6d3844f5e66784e1a8c49
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-05-29 08:49:34 +0200

    fix(core): improve typings for unknown export exception

--- packages/core/errors/exceptions/unknown-export.exception.ts
+++ packages/core/errors/exceptions/unknown-export.exception.ts
@@ -2,7 +2,7 @@ import { UNKNOWN_EXPORT_MESSAGE } from '../messages';
 import { RuntimeException } from './runtime.exception';
 
 export class UnknownExportException extends RuntimeException {
-  constructor(token: string, moduleName: string) {
+  constructor(token: string | symbol, moduleName: string) {
     super(UNKNOWN_EXPORT_MESSAGE(token, moduleName));
   }
 }
