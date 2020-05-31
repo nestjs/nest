1d15dc650ef14da81828df4f7095722f46e34f2d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-05-31 16:25:26 +0200

    sample(graphql): add complexity plugin sample

---
+++ sample/23-graphql-code-first/src/common/plugins/complexity.plugin.ts
@@ -0,0 +1,41 @@
+import { GraphQLSchemaHost, Plugin } from '@nestjs/graphql';
+import {
+  ApolloServerPlugin,
+  GraphQLRequestListener,
+} from 'apollo-server-plugin-base';
+import { GraphQLError } from 'graphql';
+import {
+  fieldExtensionsEstimator,
+  getComplexity,
+  simpleEstimator,
+} from 'graphql-query-complexity';
+
+@Plugin()
+export class ComplexityPlugin implements ApolloServerPlugin {
+  constructor(private gqlSchemaHost: GraphQLSchemaHost) {}
+
+  requestDidStart(): GraphQLRequestListener {
+    const { schema } = this.gqlSchemaHost;
+
+    return {
+      didResolveOperation({ request, document }) {
+        const complexity = getComplexity({
+          schema,
+          operationName: request.operationName,
+          query: document,
+          variables: request.variables,
+          estimators: [
+            fieldExtensionsEstimator(),
+            simpleEstimator({ defaultComplexity: 1 }),
+          ],
+        });
+        if (complexity >= 20) {
+          throw new GraphQLError(
+            `Query is too complex: ${complexity}. Maximum allowed complexity: 20`,
+          );
+        }
+        console.log('Query Complexity:', complexity);
+      },
+    };
+  }
+}
