48206c192ed02c1cfd87e172b6c9d62d9b015edc
Livio <livio.brunner.lb1@gmail.com>
2020-07-22 00:46:30 +0200

    feat: Add powered by NestJS header
    This feature adds the X-Powered-By: NestJS header
    information to every request by default
    
    fixes: #5128

--- packages/core/injector/internal-core-module.ts
+++ packages/core/injector/internal-core-module.ts
@@ -1,15 +1,28 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
-import { ValueProvider } from '@nestjs/common/interfaces';
+import {
+  ValueProvider,
+  NestModule,
+  MiddlewareConsumer,
+} from '@nestjs/common/interfaces';
 import { requestProvider } from '../router/request/request-providers';
 import { Reflector } from '../services';
 import { inquirerProvider } from './inquirer/inquirer-providers';
+
+function poweredByNestJSMiddleware(_, res, next) {
+  res.setHeader('X-Powered-By', 'NestJS');
+  next();
+}
 
 @Global()
 @Module({
   providers: [Reflector, requestProvider, inquirerProvider],
   exports: [Reflector, requestProvider, inquirerProvider],
 })
-export class InternalCoreModule {
+export class InternalCoreModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(poweredByNestJSMiddleware).forRoutes('*');
+  }
+
   static register(providers: ValueProvider[]): DynamicModule {
     return {
       module: InternalCoreModule,
