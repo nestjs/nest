5a8909bfdbfefe30a6a0a70075b59d58155fc1c1
Jay McDoniel <jmcdo29@gmail.com>
2020-10-03 11:34:36 -0700

    test(jwt-sample): adds tests to the jwt to show e2e flow works

---
+++ sample/19-auth-jwt/e2e/app/app.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { INestApplication } from '@nestjs/common';
+import { Test} from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+
+describe('E2E JWT Sample', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const modRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = modRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async() => {
+    await app.close();
+  });
+
+  it('should get a JWT then successfully make a call', async () => {
+    const loginReq = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({username: 'john', password: 'changeme'})
+      .expect(201);
+    const token = loginReq.body.access_token;
+    return request(app.getHttpServer())
+      .get('/profile')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200)
+      .expect({ userId: 1, username: 'john'})
+  });
+});
--- sample/19-auth-jwt/src/users/users.service.spec.ts
+++ sample/19-auth-jwt/src/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { expect } from 'chai';
+import { User, UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -15,4 +16,14 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  it.each`
+    name      | returnVal
+    ${'john'} | ${{ userId: 3, username: 'john', password: 'changeme' }}
+  `(
+    'should call findOne for $name and return $returnVal',
+    async ({ name, returnVal }: { name: string; returnVal: User }) => {
+      expect(await service.findOne(name)).toEqual(returnVal);
+    },
+  );
 });
+
