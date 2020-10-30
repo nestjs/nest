764859e93a9aea8f6eece4a353749da542d457ad
Patrick Münd <patrick@intracom-it.de>
2020-10-30 09:01:55 +0100

    fix(platform-fastify): adds missing chaining API types for LightMyRequest inject()

--- packages/platform-fastify/adapters/fastify-adapter.ts
+++ packages/platform-fastify/adapters/fastify-adapter.ts
@@ -190,8 +190,8 @@ export class FastifyAdapter<
     return this.instance.register(plugin, opts);
   }
 
-  public async inject(opts: InjectOptions | string) {
-    return await this.instance.inject(opts);
+  public inject(opts?: InjectOptions | string) {
+    return (opts) ? this.instance.inject(opts) : this.instance.inject();
   }
 
   public async close() {
