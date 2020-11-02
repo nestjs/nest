2344e7241813adae2294f211bb285bea4faaa24d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-11-02 10:40:20 +0100

    fix(core): when serializing dynamic module metadata, hash stringified arrow funcs

--- packages/core/injector/module-token-factory.ts
+++ packages/core/injector/module-token-factory.ts
@@ -47,7 +47,11 @@ export class ModuleTokenFactory {
 
   private replacer(key: string, value: any) {
     if (typeof value === 'function') {
-      return value.toString();
+      const isClass = /^class\s/.test(Function.prototype.toString.call(value));
+      if (isClass) {
+        return value.name;
+      }
+      return hash(value.toString(), { ignoreUnknown: true });
     }
     return value;
   }
