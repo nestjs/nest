d24d0381a225a7652f5159703b443e10fdafb395
gabeodessru <gabriel.odess@rigup.co>
2020-11-02 08:45:30 -0600

    sample(12): removed unused module method

--- integration/graphql-schema-first/src/owners/owners.module.ts
+++ integration/graphql-schema-first/src/owners/owners.module.ts
@@ -1,14 +1,7 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { OwnersService } from "./owners.service";
 
 @Module({
   providers: [OwnersService],
 })
-export class OwnersModule {
-  static enableRequestScope(): DynamicModule {
-    return {
-      module: OwnersModule,
-      providers: [OwnersService],
-    };
-  }
-}
+export class OwnersModule {}
