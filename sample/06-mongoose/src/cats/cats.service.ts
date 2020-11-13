cfd6a659c729c41bc459dbf3a368ccb4b0cc6728
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-11-13 15:17:17 +0100

    sample(06-mongoose): add cat document type

--- sample/06-mongoose/src/cats/cats.service.ts
+++ sample/06-mongoose/src/cats/cats.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCatDto } from './dto/create-cat.dto';
-import { Cat } from './schemas/cat.schema';
+import { Cat, CatDocument } from './schemas/cat.schema';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(
+    @InjectModel(Cat.name) private readonly catModel: Model<CatDocument>,
+  ) {}
 
   async create(createCatDto: CreateCatDto): Promise<Cat> {
     const createdCat = new this.catModel(createCatDto);
