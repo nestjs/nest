35add882516651b1732b0df2e2da6acca0bf37eb
Michael Kaufman <mkaufmaner@gmail.com>
2020-11-21 21:24:55 -0500

    feat(microservices): Tests for Kafka reply partitioner

--- packages/microservices/test/helpers/kafka-reply-partition-assigner.spec.ts
+++ packages/microservices/test/helpers/kafka-reply-partition-assigner.spec.ts
@@ -19,256 +19,242 @@ describe('kafka reply partition assigner', () => {
 
     getConsumerAssignments = sinon.spy(client, 'getConsumerAssignments');
     getPreviousAssignment = sinon.spy(assigner, 'getPreviousAssignment');
+
+    // reset previous assignments
+    (client as any).consumerAssignments = {};
   });
 
-  // describe('assign', () => {
-  //   it('assign all partitions evenly', async () => {
-  //     metadata['topic-A'] = Array(14)
-  //       .fill(1)
-  //       .map((_, i) => ({ partitionId: i }));
+  describe('assign', () => {
+    it('assign all partitions evenly', async () => {
+      metadata['topic-A'] = Array(14)
+        .fill(1)
+        .map((_, i) => ({ partitionId: i }));
 
-  //     metadata['topic-B'] = Array(5)
-  //       .fill(1)
-  //       .map((_, i) => ({ partitionId: i }));
+      metadata['topic-B'] = Array(5)
+        .fill(1)
+        .map((_, i) => ({ partitionId: i }));
 
-  //     const members = [
-  //       {
-  //         memberId: 'member-3',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [0, 0], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-1',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [0, 1], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-4',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [1, 1], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-2',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [2, 0], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //     ];
+      const members = [
+        {
+          memberId: 'member-3',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {},
+              }),
+            ),
+          }),
+        },
+        {
+          memberId: 'member-1',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {},
+              }),
+            ),
+          }),
+        },
+        {
+          memberId: 'member-4',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {},
+              }),
+            ),
+          }),
+        },
+        {
+          memberId: 'member-2',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {},
+              }),
+            ),
+          }),
+        },
+      ];
 
-  //     const assignment = await assigner.assign({ members, topics });
+      const assignment = await assigner.assign({ members, topics });
 
-  //     expect(assignment).to.deep.equal([
-  //       {
-  //         memberId: 'member-3',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [0, 4, 8, 12],
-  //             'topic-B': [0, 4],
-  //           },
-  //           userData: Buffer.alloc(0),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-1',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [1, 5, 9, 13],
-  //             'topic-B': [1],
-  //           },
-  //           userData: Buffer.alloc(0),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-4',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [2, 6, 10],
-  //             'topic-B': [2],
-  //           },
-  //           userData: Buffer.alloc(0),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-2',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [3, 7, 11],
-  //             'topic-B': [3],
-  //           },
-  //           userData: Buffer.alloc(0),
-  //         }),
-  //       },
-  //     ]);
-  //   });
-  // });
+      expect(assignment).to.deep.equal([
+        {
+          memberId: 'member-1',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [0, 4, 8, 12],
+              'topic-B': [0],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+        {
+          memberId: 'member-2',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [1, 5, 9, 13],
+              'topic-B': [1],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+        {
+          memberId: 'member-3',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [2, 6, 10],
+              'topic-B': [2, 4],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+        {
+          memberId: 'member-4',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [3, 7, 11],
+              'topic-B': [3],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+      ]);
+    });
+  });
 
-  // describe('re-assign', () => {
-  //   it('assign all partitions evenly', async () => {
-  //     metadata['topic-A'] = Array(4)
-  //       .fill(1)
-  //       .map((_, i) => ({ partitionId: i }));
+  describe('re-assign', () => {
+    it('assign all partitions evenly', async () => {
+      metadata['topic-A'] = Array(11)
+        .fill(1)
+        .map((_, i) => ({ partitionId: i }));
 
-  //     metadata['topic-B'] = Array(4)
-  //       .fill(1)
-  //       .map((_, i) => ({ partitionId: i }));
+      metadata['topic-B'] = Array(7)
+        .fill(1)
+        .map((_, i) => ({ partitionId: i }));
 
-  //     const members = [
-  //       {
-  //         memberId: 'member-3',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [0, 0], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-1',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [0, 1], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-4',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [1, 1], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-2',
-  //         memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
-  //           version: assigner.version,
-  //           topics: ['topic-A', 'topic-B'],
-  //           userData: Buffer.from(
-  //             JSON.stringify({
-  //               time: [2, 0], // process.hrtime()
-  //             }),
-  //           ),
-  //         }),
-  //       },
-  //     ];
+      const members = [
+        {
+          memberId: 'member-3',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {
+                  'topic-A': 0,
+                  'topic-B': 0,
+                },
+              }),
+            ),
+          }),
+        },
+        {
+          memberId: 'member-1',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {
+                  'topic-A': 1,
+                  'topic-B': 1,
+                },
+              }),
+            ),
+          }),
+        },
+        {
+          memberId: 'member-4',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {
+                  'topic-A': 2,
+                },
+              }),
+            ),
+          }),
+        },
+        {
+          memberId: 'member-2',
+          memberMetadata: Kafka.AssignerProtocol.MemberMetadata.encode({
+            version: assigner.version,
+            topics: ['topic-A', 'topic-B'],
+            userData: Buffer.from(
+              JSON.stringify({
+                previousAssignment: {},
+              }),
+            ),
+          }),
+        },
+      ];
 
-  //     const assignment = await assigner.assign({ members, topics });
+      const assignment = await assigner.assign({ members, topics });
 
-  //     const simpleAssignment = Buffer.from(
-  //       JSON.stringify({
-  //         assignment: {
-  //           'member-3': {
-  //             'topic-A': [0],
-  //             'topic-B': [0],
-  //           },
-  //           'member-1': {
-  //             'topic-A': [1],
-  //             'topic-B': [1],
-  //           },
-  //           'member-4': {
-  //             'topic-A': [2],
-  //             'topic-B': [2],
-  //           },
-  //           'member-2': {
-  //             'topic-A': [3],
-  //             'topic-B': [3],
-  //           },
-  //         },
-  //       }),
-  //     );
-
-  //     console.log(assignment);
-
-  //     expect(assignment).to.deep.equal([
-  //       {
-  //         memberId: 'member-3',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [0],
-  //             'topic-B': [0],
-  //           },
-  //           userData: simpleAssignment,
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-1',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [1],
-  //             'topic-B': [1],
-  //           },
-  //           userData: simpleAssignment,
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-4',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [2],
-  //             'topic-B': [2],
-  //           },
-  //           userData: simpleAssignment,
-  //         }),
-  //       },
-  //       {
-  //         memberId: 'member-2',
-  //         memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
-  //           version: assigner.version,
-  //           assignment: {
-  //             'topic-A': [3],
-  //             'topic-B': [3],
-  //           },
-  //           userData: simpleAssignment,
-  //         }),
-  //       },
-  //     ]);
-  //   });
-  // });
+      expect(assignment).to.deep.equal([
+        {
+          memberId: 'member-1',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [1, 4, 8],
+              'topic-B': [1, 5],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+        {
+          memberId: 'member-2',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [3, 5, 9],
+              'topic-B': [2, 6],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+        {
+          memberId: 'member-3',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [0, 6, 10],
+              'topic-B': [0],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+        {
+          memberId: 'member-4',
+          memberAssignment: Kafka.AssignerProtocol.MemberAssignment.encode({
+            version: assigner.version,
+            assignment: {
+              'topic-A': [2, 7],
+              'topic-B': [3, 4],
+            },
+            userData: Buffer.alloc(0),
+          }),
+        },
+      ]);
+    });
+  });
 
   describe('protocol', () => {
     it('returns the assigner name and metadata', () => {
