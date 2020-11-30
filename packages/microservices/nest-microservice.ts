48690903d2f99273a3b4e362eea0dd408cbd8b6a
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2020-11-30 08:46:16 +0100

    Update packages/microservices/nest-microservice.ts

--- packages/microservices/nest-microservice.ts
+++ packages/microservices/nest-microservice.ts
@@ -115,7 +115,7 @@ export class NestMicroservice
     return this;
   }
 
-  public listen(callback: () => void): void {
+  public listen(callback: () => void) {
     this.listenAsync().then(callback);
   }
 
