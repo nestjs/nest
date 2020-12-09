8d3bf8e461c08c3e7105f0b900c266db1b20f513
Mahdi <mahdighajary@gmail.com>
2020-12-09 17:37:16 +0330

    refactor(platform-express): expose Multer file type

--- packages/platform-express/multer/interfaces/multer-options.interface.ts
+++ packages/platform-express/multer/interfaces/multer-options.interface.ts
@@ -1,6 +1,27 @@
 /**
  * @see https://github.com/expressjs/multer
  */
+
+ export interface MulterFile {
+    /** Field name specified in the form */
+    fieldname: string;
+    /** Name of the file on the user's computer */
+    originalname: string;
+    /** Encoding type of the file */
+    encoding: string;
+    /** Mime type of the file */
+    mimetype: string;
+    /** Size of the file in bytes */
+    size: number;
+    /** The folder to which the file has been saved (DiskStorage) */
+    destination: string;
+    /** The name of the file within the destination (DiskStorage) */
+    filename: string;
+    /** Location of the uploaded file (DiskStorage) */
+    path: string;
+    /** A Buffer of the entire file (MemoryStorage) */
+    buffer: Buffer;
+ }
 export interface MulterOptions {
   dest?: string;
   /** The storage engine to use for uploaded files. */
@@ -31,26 +52,7 @@ export interface MulterOptions {
 
   fileFilter?(
     req: any,
-    file: {
-      /** Field name specified in the form */
-      fieldname: string;
-      /** Name of the file on the user's computer */
-      originalname: string;
-      /** Encoding type of the file */
-      encoding: string;
-      /** Mime type of the file */
-      mimetype: string;
-      /** Size of the file in bytes */
-      size: number;
-      /** The folder to which the file has been saved (DiskStorage) */
-      destination: string;
-      /** The name of the file within the destination (DiskStorage) */
-      filename: string;
-      /** Location of the uploaded file (DiskStorage) */
-      path: string;
-      /** A Buffer of the entire file (MemoryStorage) */
-      buffer: Buffer;
-    },
+    file: MulterFile,
     callback: (error: Error | null, acceptFile: boolean) => void,
   ): void;
 }
