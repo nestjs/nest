da371c7d491d036e6345893ac0e114e32f14fedc
Alexander Kaufmann <ak@vivid-planet.com>
2021-01-25 09:07:26 +0100

    feat(@nestjs/common): add pipe transformer for parsing floats

--- packages/common/pipes/index.ts
+++ packages/common/pipes/index.ts
@@ -2,5 +2,6 @@ export * from './default-value.pipe';
 export * from './parse-array.pipe';
 export * from './parse-bool.pipe';
 export * from './parse-int.pipe';
+export * from './parse-float.pipe';
 export * from './parse-uuid.pipe';
 export * from './validation.pipe';
