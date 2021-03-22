2208a874fc10a7955e49786448becd1e3fc2c37f
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2021-03-22 10:41:27 +0100

    fix(microservices): when host present, ignore default url (redis)

--- packages/microservices/client/client-redis.ts
+++ packages/microservices/client/client-redis.ts
@@ -30,7 +30,11 @@ export class ClientRedis extends ClientProxy {
 
   constructor(protected readonly options: RedisOptions['options']) {
     super();
-    this.url = this.getOptionsProp(options, 'url') || REDIS_DEFAULT_URL;
+    this.url =
+      this.getOptionsProp(options, 'url') ??
+      this.getOptionsProp(options, 'host')
+        ? undefined
+        : REDIS_DEFAULT_URL;
 
     redisPackage = loadPackage('redis', ClientRedis.name, () =>
       require('redis'),
--- packages/microservices/server/server-redis.ts
+++ packages/microservices/server/server-redis.ts
@@ -30,7 +30,11 @@ export class ServerRedis extends Server implements CustomTransportStrategy {
 
   constructor(private readonly options: RedisOptions['options']) {
     super();
-    this.url = this.getOptionsProp(this.options, 'url') || REDIS_DEFAULT_URL;
+    this.url =
+      this.getOptionsProp(options, 'url') ??
+      this.getOptionsProp(options, 'host')
+        ? undefined
+        : REDIS_DEFAULT_URL;
 
     redisPackage = this.loadPackage('redis', ServerRedis.name, () =>
       require('redis'),
