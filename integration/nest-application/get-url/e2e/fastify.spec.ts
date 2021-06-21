59e0d0ba90443b983d77c1057319f02bde895f04
Eric Haynes <ehaynes@better.com>
2021-06-21 13:30:26 -0400

    test(core) Add failing tests demonstrating getUrl bug

--- integration/nest-application/get-url/e2e/fastify.spec.ts
+++ integration/nest-application/get-url/e2e/fastify.spec.ts
@@ -1,50 +1,53 @@
 import { FastifyAdapter } from '@nestjs/platform-fastify';
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
 import { expect } from 'chai';
 import { AppModule } from '../src/app.module';
 import { randomPort } from './utils';
 
 describe('Get URL (Fastify Application)', () => {
-  let testModule: TestingModule;
   let port: number;
-
-  beforeEach(async () => {
-    testModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-  });
+  let app: INestApplication;
 
   beforeEach(async () => {
     port = await randomPort();
+    const testModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = testModule.createNestApplication(new FastifyAdapter());
+  });
+
+  afterEach(async () => {
+    app && (await app.close());
   });
 
   it('should be able to get the IPv4 address', async () => {
-    const app = testModule.createNestApplication(new FastifyAdapter());
-    await app.listen(port, '127.0.0.5');
-    expect(await app.getUrl()).to.be.eql(`http://127.0.0.5:${port}`);
-    await app.close();
+    await app.listen(port, '127.0.0.1');
+    expect(await app.getUrl()).to.be.eql(`http://127.0.0.1:${port}`);
   });
   it('should return 127.0.0.1 for 0.0.0.0', async () => {
-    const app = testModule.createNestApplication(new FastifyAdapter());
     await app.listen(port, '0.0.0.0');
     expect(await app.getUrl()).to.be.eql(`http://127.0.0.1:${port}`);
-    await app.close();
   });
-  it('should return 127.0.0.1 even in a callback', () => {
-    const app = testModule.createNestApplication(new FastifyAdapter());
-    return app.listen(port, async () => {
+  it('should be able to get the IPv4 address after app is listening', async () => {
+    await app.listenAsync(port, '0.0.0.0');
+    expect(await app.getUrl()).to.be.eql(`http://127.0.0.1:${port}`);
+  });
+  it('should be able to get the IPv4 address in a callback', done => {
+    app.listen(port, '0.0.0.0', async () => {
       expect(await app.getUrl()).to.be.eql(`http://127.0.0.1:${port}`);
-      await app.close();
+      done();
     });
   });
   it('should throw an error for calling getUrl before listen', async () => {
-    const app = testModule.createNestApplication(new FastifyAdapter());
+    let err: any;
     try {
       await app.getUrl();
-    } catch (err) {
-      expect(err).to.be.eql(
-        'app.listen() needs to be called before calling app.getUrl()',
-      );
+    } catch (error) {
+      err = error;
     }
+    expect(err).to.be.eql(
+      'app.listen() needs to be called before calling app.getUrl()',
+    );
   });
 });
