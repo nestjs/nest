674dd56d32e76e37021b5e08ab7f2e081078f428
alex p <neo140@gmail.com>
2021-06-29 18:12:35 +0300

    fix(microservices): fixed bug that socketOptions object not passed to amqp connection manager instance

--- packages/microservices/server/server-rmq.ts
+++ packages/microservices/server/server-rmq.ts
@@ -99,7 +99,11 @@ export class ServerRMQ extends Server implements CustomTransportStrategy {
 
   public createClient<T = any>(): T {
     const socketOptions = this.getOptionsProp(this.options, 'socketOptions');
-    return rqmPackage.connect(this.urls, { connectionOptions: socketOptions });
+    return rqmPackage.connect(this.urls, { 
+        connectionOptions: socketOptions,
+        heartbeatIntervalInSeconds: socketOptions?.heartbeatIntervalInSeconds,
+        reconnectTimeInSeconds: socketOptions?.reconnectTimeInSeconds
+    });
   }
 
   public async setupChannel(channel: any, callback: Function) {
