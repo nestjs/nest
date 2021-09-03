9064a29081fb6e2ef491aa78214b3979b56c3cfd
Jay McDoniel <jmcdo29@gmail.com>
2021-09-03 13:17:12 -0700

    chore: remove logger change in cats sample

--- sample/01-cats-app/src/main.ts
+++ sample/01-cats-app/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: console });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
