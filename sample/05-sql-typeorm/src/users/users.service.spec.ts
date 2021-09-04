ac3f458c56e19e58a58b03ec63605f33814c7124
Tony133 <a.tripodi133@gmail.com>
2021-09-04 21:28:22 +0200

    test(sample-05): added unit tests

---
+++ sample/05-sql-typeorm/src/users/users.service.spec.ts
@@ -0,0 +1,83 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+
+const userArray = [
+  {
+    firstName: 'firstName #1',
+    lastName: 'lastName #1',
+  },
+  {
+    firstName: 'firstName #2',
+    lastName: 'lastName #2',
+  },
+];
+
+const oneUser = {
+  firstName: 'firstName #1',
+  lastName: 'lastName #1',
+};
+
+describe('UserService', () => {
+  let service: UsersService;
+  let repository: Repository<User>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            find: jest.fn().mockResolvedValue(userArray),
+            findOne: jest.fn().mockResolvedValue(oneUser),
+            save: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should successfully insert a user', () => {
+      const user = {
+        firstName: 'firstName #1',
+        lastName: 'lastName #1',
+      };
+      expect(user).toEqual(user);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should return an array of users', async () => {
+      const users = await service.findAll();
+      expect(users).toEqual(userArray);
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should get a single user', () => {
+      const repoSpy = jest.spyOn(repository, 'findOne');
+      expect(service.findOne('a id')).resolves.toEqual(oneUser);
+      expect(repoSpy).toBeCalledWith('a id');
+    });
+  });
+
+  describe('remove()', () => {
+    it('should remove a user', async () => {
+      const removeUserSpy = jest.spyOn(service, 'delete');
+      service.delete('anyid');
+      expect(removeUserSpy).toHaveBeenCalledWith('anyid');
+    });
+  });
+});
