06fd14514757e9c6b0c777506c3b258efa3d907b
Tony133 <a.tripodi133@gmail.com>
2021-10-21 11:01:03 +0200

    chore(): removed unused import

--- sample/07-sequelize/src/app.module.ts
+++ sample/07-sequelize/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from './users/models/user.model';
 import { UsersModule } from './users/users.module';
 
 @Module({
