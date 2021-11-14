88ec63906a8bed9b85e6554466218f943cd5ac34
joao.furtado@mercafacil.me <joao.furtado@mercafacil.me>
2021-11-14 01:52:24 -0300

    docs(samples): Adding the plugins on the GraphQL sample project

--- sample/23-graphql-code-first/src/app.module.ts
+++ sample/23-graphql-code-first/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RecipesModule } from './recipes/recipes.module';
+import { ComplexityPlugin } from './common/plugins/complexity.plugin';
+import { LoggingPlugin } from './common/plugins/logging.plugin';
 
 @Module({
+  providers: [ComplexityPlugin, LoggingPlugin],
   imports: [
     RecipesModule,
     GraphQLModule.forRoot({
