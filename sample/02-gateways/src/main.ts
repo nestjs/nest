1a35773d4eff52694be105891f0a9d434246e8f7
Abu Bakar Nawaz Ghumman <44497582+ABNG@users.noreply.github.com>
2021-11-20 21:11:44 +0500

    Update main.ts
    added cors

--- sample/02-gateways/src/main.ts
+++ sample/02-gateways/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
