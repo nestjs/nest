bfe4868557cf85e69f0864bae1f3b0eaa81226f1
Abu Bakar Nawaz Ghumman <44497582+ABNG@users.noreply.github.com>
2021-11-22 23:09:55 +0500

    Update main.ts
    remove enableCors() line.

--- sample/02-gateways/src/main.ts
+++ sample/02-gateways/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
