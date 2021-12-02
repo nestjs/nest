9683ac2502d923861c9cc0cb3722f4619dc04ec4
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-12-02 02:32:16 -0400

    test(core,common): add unit tests for duplicated versions scenario

--- packages/common/test/decorators/version.decorator.spec.ts
+++ packages/common/test/decorators/version.decorator.spec.ts
@@ -4,14 +4,27 @@ import { Version } from '../../decorators/core/version.decorator';
 
 describe('@Version', () => {
   const version = '1';
+  const versions = ['1', '2', '2', '1', '2', '1'];
+  const versionsWithoutDuplicates = ['1', '2'];
 
   class Test {
     @Version(version)
-    public static test() {}
+    public static oneVersion() {}
+
+    @Version(versions)
+    public static multipleVersions() {}
   }
 
   it('should enhance method with expected version string', () => {
-    const metadata = Reflect.getMetadata(VERSION_METADATA, Test.test);
+    const metadata = Reflect.getMetadata(VERSION_METADATA, Test.oneVersion);
     expect(metadata).to.be.eql(version);
+  });
+
+  it('should enhance method with expected version array', () => {
+    const metadata = Reflect.getMetadata(
+      VERSION_METADATA,
+      Test.multipleVersions,
+    );
+    expect(metadata).to.be.eql(versionsWithoutDuplicates);
   });
 });
