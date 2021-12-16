11ae6e11e27fb1dfad61fd1770bcac8f56f33fbd
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2021-12-16 14:08:09 +0100

    Update packages/core/errors/messages.ts

--- packages/core/errors/messages.ts
+++ packages/core/errors/messages.ts
@@ -125,7 +125,7 @@ export const USING_INVALID_CLASS_AS_A_MODULE_MESSAGE = (
 
   // TODO(v9): Edit the message below:
   return `In the next major version, Nest will not allow classes annotated with @Injectable(), @Catch(), and @Controller() decorators to appear in the "imports" array of a module.
-Please remove ${metatypeName} (including forwarded occurrences, if any) from all of "imports" arrays.
+Please remove "${metatypeName}" (including forwarded occurrences, if any) from all of the "imports" arrays.
 
 Scope [${stringifyScope(scope)}]
 `;
