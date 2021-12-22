0fb3233e57caed3ba552c07dadc567499d8a043c
Tony133 <a.tripodi133@gmail.com>
2021-12-22 11:49:44 +0100

    sample(05-sql-typeorm): update dependecie for mysql 8.x and some improvements

--- sample/05-sql-typeorm/src/main.ts
+++ sample/05-sql-typeorm/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
