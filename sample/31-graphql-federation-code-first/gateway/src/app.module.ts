d8ba5d16e6e9abcc3c7e1dbe3951bbeee6d0044b
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2022-02-04 12:17:40 +0100

    sample(): migrate graphql samples to the latest version of @nestjs/graphql

--- sample/31-graphql-federation-code-first/gateway/src/app.module.ts
+++ sample/31-graphql-federation-code-first/gateway/src/app.module.ts
@@ -1,19 +1,21 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLGatewayModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    GraphQLGatewayModule.forRoot({
-      server: {
-        cors: true,
-      },
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
       gateway: {
-        serviceList: [
-          { name: 'users', url: 'http://localhost:3000/graphql' },
-          { name: 'posts', url: 'http://localhost:3001/graphql' },
-        ],
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'users', url: 'http://localhost:3000/graphql' },
+            { name: 'posts', url: 'http://localhost:3001/graphql' },
+          ],
+        }),
       },
     }),
   ],
