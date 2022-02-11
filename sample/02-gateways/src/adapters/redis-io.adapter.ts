2f439b6f38f1ecc1555da1cf82d41fb5891f285d
Tony133 <a.tripodi133@gmail.com>
2022-02-11 12:38:45 +0100

    sample(02-gateways): update dependencies

--- sample/02-gateways/src/adapters/redis-io.adapter.ts
+++ sample/02-gateways/src/adapters/redis-io.adapter.ts
@@ -1,12 +1,12 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { RedisClient } from 'redis';
-import { ServerOptions } from 'socket.io';
-import { createAdapter } from 'socket.io-redis';
+import { Server, ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
 
-const pubClient = new RedisClient({ host: 'localhost', port: 6379 });
+const io = new Server();
+const pubClient = createClient({ url: `redis://localhost:6379` });
 const subClient = pubClient.duplicate();
-const redisAdapter = createAdapter({ pubClient, subClient });
-
+const redisAdapter = io.adapter(createAdapter(pubClient, subClient));
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
