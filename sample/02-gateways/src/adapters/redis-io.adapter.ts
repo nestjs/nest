f2766186d31c9ef34352689b6f434b02aea5a5b6
Tony133 <a.tripodi133@gmail.com>
2022-02-15 13:23:57 +0100

    sample(02-gateways): update

--- sample/02-gateways/src/adapters/redis-io.adapter.ts
+++ sample/02-gateways/src/adapters/redis-io.adapter.ts
@@ -6,6 +6,10 @@ import { createClient } from 'redis';
 const io = new Server();
 const pubClient = createClient({ url: `redis://localhost:6379` });
 const subClient = pubClient.duplicate();
+
+pubClient.connect();
+subClient.connect();
+
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
