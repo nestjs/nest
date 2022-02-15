d2f0cf03f64bb2b75df62ff3c92e1b05f09f1ee5
Tony133 <a.tripodi133@gmail.com>
2022-02-15 13:17:58 +0100

    sample(02-gateway): fix redis adapter

--- sample/02-gateways/src/adapters/redis-io.adapter.ts
+++ sample/02-gateways/src/adapters/redis-io.adapter.ts
@@ -6,11 +6,10 @@ import { createClient } from 'redis';
 const io = new Server();
 const pubClient = createClient({ url: `redis://localhost:6379` });
 const subClient = pubClient.duplicate();
-const redisAdapter = io.adapter(createAdapter(pubClient, subClient));
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
-    server.adapter(redisAdapter);
+    server.adapter(createAdapter(pubClient, subClient));
     return server;
   }
 }
