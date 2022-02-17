ff66f4cf8e18dcba07eeafea6407e5c67f3d4224
Tomer Ron <tomer@tomerron.com>
2022-02-17 13:07:33 +0100

    docs(sample-02-gateways): remove redis adapter, add documentation on how to add the redis adapter

--- sample/02-gateways/src/main.ts
+++ sample/02-gateways/src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { RedisIoAdapter } from './adapters/redis-io.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
-
-  app.useWebSocketAdapter(redisIoAdapter);
+  // Uncomment these lines to use the Redis adapter:
+  // const redisIoAdapter = new RedisIoAdapter(app);
+  // await redisIoAdapter.connectToRedis();
+  // app.useWebSocketAdapter(redisIoAdapter);
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
