6136e46419e09b5098fb9ea3ebbef18e77371312
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2022-02-18 16:59:24 +0100

    feat(core): support optional dependencies in factory providers

---
+++ packages/common/interfaces/modules/injection-token.interface.ts
@@ -0,0 +1,9 @@
+import { Abstract } from '../abstract.interface';
+import { Type } from '../type.interface';
+
+export type InjectionToken =
+  | string
+  | symbol
+  | Type<any>
+  | Abstract<any>
+  | Function;
