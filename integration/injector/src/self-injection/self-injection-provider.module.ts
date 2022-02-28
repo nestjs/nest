560b95c645e93e1b642a0b7b4441bba27a6581d5
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-02-28 18:18:45 -0400

    test(integration): add tests for self-injections providers

---
+++ integration/injector/src/self-injection/self-injection-provider.module.ts
@@ -0,0 +1,40 @@
+import { Module, Injectable, Inject, forwardRef } from '@nestjs/common';
+
+@Injectable()
+class ServiceInjectingItself {
+  constructor(private readonly coreService: ServiceInjectingItself) {}
+}
+
+@Injectable()
+class ServiceInjectingItselfForwared {
+  constructor(
+    @Inject(forwardRef(() => ServiceInjectingItself))
+    private readonly coreService: ServiceInjectingItself,
+  ) {}
+}
+
+@Injectable()
+class ServiceInjectingItselfViaCustomToken {
+  constructor(@Inject('AnotherToken') private readonly coreService: any) {}
+}
+
+@Module({
+  providers: [ServiceInjectingItself],
+})
+export class SelfInjectionProviderModule {}
+
+@Module({
+  providers: [ServiceInjectingItselfForwared],
+})
+export class SelfInjectionForwardProviderModule {}
+
+@Module({
+  providers: [
+    ServiceInjectingItselfViaCustomToken,
+    {
+      provide: 'AnotherToken',
+      useClass: ServiceInjectingItselfViaCustomToken,
+    },
+  ],
+})
+export class SelfInjectionProviderCustomTokenModule {}
