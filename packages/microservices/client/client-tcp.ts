aeb315809ec1ef83127f75868dd2a39fb098b25a
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2022-03-07 13:52:58 +0100

    fix(microservices): tcp client parallel connections issue

--- packages/microservices/client/client-tcp.ts
+++ packages/microservices/client/client-tcp.ts
@@ -36,7 +36,7 @@ export class ClientTCP extends ClientProxy {
   }
 
   public connect(): Promise<any> {
-    if (this.isConnected && this.connection) {
+    if (this.connection) {
       return this.connection;
     }
     this.socket = this.createSocket();
