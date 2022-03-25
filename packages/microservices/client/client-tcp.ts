60f09913af8cd9d4583611c0b8dd53c202b00721
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2022-03-25 16:11:28 +0100

    fix(microservices): fix tcp client reconnection issue #9363

--- packages/microservices/client/client-tcp.ts
+++ packages/microservices/client/client-tcp.ts
@@ -107,6 +107,7 @@ export class ClientTCP extends ClientProxy {
   public handleClose() {
     this.isConnected = false;
     this.socket = null;
+    this.connection = undefined;
   }
 
   protected publish(
