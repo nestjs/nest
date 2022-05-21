2ce994dbd04ed6ddcc3aca354935b1e03c755be8
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-05-21 19:11:45 -0400

    chore: keep only one eslint config file for dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+  root: true,
+  env: {
+    node: true,
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -10,16 +9,39 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  root: true,
-  env: {
-    node: true,
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/ban-types': 'off',
-  },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-types': 'off',
+      },
+    },
+    {
+      files: ['**/*.spec.ts', 'integration/**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.spec.json',
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+      },
+    }
+  ]
 };
