9c925ff0743dd205ba302affff47b1ab17547fb5
Tony133 <a.tripodi133@gmail.com>
2022-06-01 18:10:21 +0200

    sample(05-sql-typeorm): update unit test

--- sample/05-sql-typeorm/src/users/users.service.spec.ts
+++ sample/05-sql-typeorm/src/users/users.service.spec.ts
@@ -76,7 +76,7 @@ describe('UserService', () => {
     it('should get a single user', () => {
       const repoSpy = jest.spyOn(repository, 'findBy');
       expect(service.findOne('1')).resolves.toEqual(oneUser);
-      expect(repoSpy).toBeCalledWith('1');
+      expect(repoSpy).toBeCalledWith({ id: 1 });
     });
   });
 
