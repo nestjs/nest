231b9f6b3efb175c009ac0eb125adb5f148ea17d
Thiago Martins <thiago_valentim1@hotmail.com>
2022-06-24 18:26:44 -0300

    feat(common): file type validator
    add regex support when checking a file mime-type

--- packages/common/pipes/file/file-type.validator.ts
+++ packages/common/pipes/file/file-type.validator.ts
@@ -1,11 +1,15 @@
 import { FileValidator } from './file-validator.interface';
 
 export type FileTypeValidatorOptions = {
-  fileType: string;
+  fileType: string | RegExp;
 };
 
 /**
- * Defines the built-in FileType File Validator
+ * Defines the built-in FileType File Validator. It validates incoming files mime-type
+ * matching a string or a regular expression. Note that this validator uses a naive strategy
+ * to check the mime-type and could be fooled if the client provided a file with renamed extension.
+ * (for instance, renaming a 'malicious.bat' to 'malicious.jpeg'). To handle such security issues
+ * with more reliability, consider checking against the file's [magic-numbers](https://en.wikipedia.org/wiki/Magic_number_%28programming%29)
  *
  * @see [File Validators](https://docs.nestjs.com/techniques/file-upload#validators)
  *
@@ -25,6 +29,8 @@ export class FileTypeValidator extends FileValidator<FileTypeValidatorOptions> {
       return false;
     }
 
-    return (file.mimetype as string).endsWith(this.validationOptions.fileType);
+    return Boolean(
+      (file.mimetype as string).match(this.validationOptions.fileType),
+    );
   }
 }
