080e09c86dffcc428133a99e77b4da98acd8e406
Avni Genç <avnignc@gmail.com>
2022-08-17 00:58:51 +0300

    feat(microservice): add grpc status enum

---
+++ packages/microservices/enums/grpc-status.enum.ts
@@ -0,0 +1,22 @@
+/**
+ * @see https://grpc.github.io/grpc/core/md_doc_statuscodes.html
+ */
+export enum GrpcStatus {
+  OK = 0,
+  CANCELLED = 1,
+  UNKNOWN = 2,
+  INVALID_ARGUMENT = 3,
+  DEADLINE_EXCEEDED = 4,
+  NOT_FOUND = 5,
+  ALREADY_EXISTS = 6,
+  PERMISSION_DENIED = 7,
+  RESOURCE_EXHAUSTED = 8,
+  FAILED_PRECONDITION = 9,
+  ABORTED = 10,
+  OUT_OF_RANGE = 11,
+  UNIMPLEMENTED = 12,
+  INTERNAL = 13,
+  UNAVAILABLE = 14,
+  DATA_LOSS = 15,
+  UNAUTHENTICATED = 16,
+}
