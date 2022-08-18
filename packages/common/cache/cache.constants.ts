3cd1b1d95b7becf68bbcbc89f1ff4c7adc1519f2
Jay McDoniel <jmcdo29@gmail.com>
2022-08-18 12:47:22 -0700

    fix: export cache module options token under easy name

--- packages/common/cache/cache.constants.ts
+++ packages/common/cache/cache.constants.ts
@@ -1,3 +1,6 @@
+import { MODULE_OPTIONS_TOKEN } from './cache.module-definition';
+
 export const CACHE_MANAGER = 'CACHE_MANAGER';
 export const CACHE_KEY_METADATA = 'cache_module:cache_key';
 export const CACHE_TTL_METADATA = 'cache_module:cache_ttl';
+export const CACHE_MODULE_OPTIONS = MODULE_OPTIONS_TOKEN;
