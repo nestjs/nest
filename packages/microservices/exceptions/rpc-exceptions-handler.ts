469580da635a825d434e73594089cbcace35d1d1
Pooyan Khanjankhani <p.khani@tejaratshayan.com>
2022-08-18 23:07:10 +0430

    refactor(microservices): use `selectExceptionFilterMetadata`

--- packages/microservices/exceptions/rpc-exceptions-handler.ts
+++ packages/microservices/exceptions/rpc-exceptions-handler.ts
@@ -1,10 +1,11 @@
-import { RpcExceptionFilterMetadata } from '@nestjs/common/interfaces/exceptions';
-import { ArgumentsHost } from '@nestjs/common/interfaces/features/arguments-host.interface';
 import { isEmpty } from '@nestjs/common/utils/shared.utils';
-import { InvalidExceptionFilterException } from '@nestjs/core/errors/exceptions/invalid-exception-filter.exception';
 import { Observable } from 'rxjs';
-import { BaseRpcExceptionFilter } from './base-rpc-exception-filter';
+import { ArgumentsHost } from '@nestjs/common/interfaces/features/arguments-host.interface';
+import { RpcExceptionFilterMetadata } from '@nestjs/common/interfaces/exceptions';
+import { InvalidExceptionFilterException } from '@nestjs/core/errors/exceptions/invalid-exception-filter.exception';
+import { selectExceptionFilterMetadata } from '@nestjs/common/utils/select-exception-filter-metadata';
 import { RpcException } from './rpc-exception';
+import { BaseRpcExceptionFilter } from './base-rpc-exception-filter';
 
 export class RpcExceptionsHandler extends BaseRpcExceptionFilter {
   private filters: RpcExceptionFilterMetadata[] = [];
@@ -34,14 +35,8 @@ export class RpcExceptionsHandler extends BaseRpcExceptionFilter {
     if (isEmpty(this.filters)) {
       return null;
     }
-    const filter = this.filters.find(({ exceptionMetatypes }) => {
-      const hasMetatype =
-        !exceptionMetatypes.length ||
-        exceptionMetatypes.some(
-          ExceptionMetatype => exception instanceof ExceptionMetatype,
-        );
-      return hasMetatype;
-    });
+
+    const filter = selectExceptionFilterMetadata(this.filters, exception);
     return filter ? filter.func(exception, host) : null;
   }
 }
