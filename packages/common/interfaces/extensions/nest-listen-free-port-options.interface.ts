d4a7cfd912f17937b95c2810a228facb54423c4b
Andrey Kirik <dev.andrey.kirik@gmail.com>
2022-10-14 00:53:23 +0600

    docs: add docs for listen-free-port-options interface

--- packages/common/interfaces/extensions/nest-listen-free-port-options.interface.ts
+++ packages/common/interfaces/extensions/nest-listen-free-port-options.interface.ts
@@ -1,5 +1,19 @@
 export interface INestListenFreePortOptions {
+  /**
+   * Port.
+   * */
   port: string | number;
+  /**
+   * Callback function that call when port was busy.
+   *
+   * @param {number} port Busy port.
+   * @returns {boolean} Continue finding free port? true - to continue, false - to reject listening with Error.
+   * */
   onSkip: (port: number) => boolean;
-  onStart: (port: number) => boolean;
+  /**
+   * Callback function that call when server starts listening.
+   *
+   * @param {number} port Final selected port.
+   * */
+  onStart: (port: number) => void;
 }
