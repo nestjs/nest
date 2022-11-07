d1d8cf6c59edefb0c94200703de98c0811ba2aac
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2022-11-07 14:16:27 +0100

    chore(scripts): only move .d.ts and .js files (gulp scripts)

--- tools/gulp/tasks/move.ts
+++ tools/gulp/tasks/move.ts
@@ -3,7 +3,11 @@ import { join } from 'path';
 import { samplePath } from '../config';
 import { containsPackageJson, getDirs } from '../util/task-helpers';
 
-const distFiles = src(['packages/**/*']);
+const distFiles = src([
+  'packages/**/*',
+  '!packages/**/*.ts',
+  'packages/**/*.d.ts',
+]);
 
 /**
  * Moves the compiled nest files into "node_module" folder.
