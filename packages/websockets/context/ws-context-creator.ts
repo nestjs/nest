c29a919e05f883502d964903c250fdb5c050a276
Jay McDoniel <jmcdo29@gmail.com>
2022-11-09 13:59:04 -0800

    ci:  don't install greater than npm 8

--- packages/websockets/context/ws-context-creator.ts
+++ packages/websockets/context/ws-context-creator.ts
@@ -109,6 +109,7 @@ export class WsContextCreator {
     };
 
     return this.wsProxy.create(async (...args: unknown[]) => {
+      console.log(args);
       args.push(this.reflectCallbackPattern(callback));
       const initialArgs = this.contextUtils.createNullArray(argsLength);
       fnCanActivate && (await fnCanActivate(args));
