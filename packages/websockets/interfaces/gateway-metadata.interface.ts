b683ae9e503560cfe33031cea3fcf917e30a2040
Manuel Herrera <manuel.herrera@owasp.org>
2022-11-24 09:42:30 -0300

    feat(websockets): add prefix option for api versioning

--- packages/websockets/interfaces/gateway-metadata.interface.ts
+++ packages/websockets/interfaces/gateway-metadata.interface.ts
@@ -15,6 +15,11 @@ export interface GatewayMetadata {
    */
   path?: string;
   /**
+   * Api version acts as prefix to path
+   * @default undefined
+   */
+  prefix?: string;
+  /**
    * Whether to serve the client files
    * @default true
    */
