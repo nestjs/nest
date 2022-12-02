c817014c7dfe67e5f985c2b94ccb7ffcf6505253
e-dot <e-dot@users.noreply.github.com>
2022-12-02 18:48:21 +0100

    Windows IIS Compatibility : listen(FastifyListenOptions)
    Adapt the fastify adapter to be compatible with Microsoft IIS : requires the new listen() API with a FastifyListenOptions parameter

--- packages/platform-fastify/adapters/fastify-adapter.ts
+++ packages/platform-fastify/adapters/fastify-adapter.ts
@@ -32,6 +32,7 @@ import {
   RawServerBase,
   RawServerDefault,
   RequestGenericInterface,
+  FastifyListenOptions,
 } from 'fastify';
 import * as Reply from 'fastify/lib/reply';
 import { RouteShorthandMethod } from 'fastify/types/route';
@@ -226,12 +227,19 @@ export class FastifyAdapter<
     hostname: string,
     callback?: () => void,
   ): void;
-  public listen(port: string | number, ...args: any[]): void {
+  public listen(listenOptions: string | number | FastifyListenOptions, ...args: any[]): void {
     const isFirstArgTypeofFunction = typeof args[0] === 'function';
     const callback = isFirstArgTypeofFunction ? args[0] : args[1];
-    const options: Record<string, any> = {
-      port: +port,
-    };
+    let options: Record<string, any> = {}
+    if (typeof(listenOptions) == 'object' && (listenOptions.host || listenOptions.port || listenOptions.path)) {
+        // Handle new function signature : first parameter is an object with path, port and/or host attributes
+        options = listenOptions;
+    } else {
+        // Old signature - first parameter MUST be an integer (port number)
+        options = {
+            port: +listenOptions
+        };
+    }
     if (!isFirstArgTypeofFunction) {
       options.host = args[0];
     }
