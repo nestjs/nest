0b4d7f82a7462ff603d9e4073e8ded5bd4621894
Microwavekonijn <microwavekonijn@pm.me>
2023-01-10 21:24:12 +0100

    fix(test): extend ws factory test

--- packages/websockets/test/factories/server-and-event-streams-factory.spec.ts
+++ packages/websockets/test/factories/server-and-event-streams-factory.spec.ts
@@ -1,17 +1,25 @@
 import { expect } from 'chai';
 import { ReplaySubject, Subject } from 'rxjs';
 import { ServerAndEventStreamsFactory } from '../../factories/server-and-event-streams-factory';
+import { AbstractWsAdapter } from '@nestjs/websockets';
+
+class NoopAdapter extends AbstractWsAdapter {
+  public create(port: number, options?: any) {}
+  public bindMessageHandlers(client: any, handlers) {}
+}
 
 describe('ServerAndEventStreamsFactory', () => {
   describe('create', () => {
     it(`should return expected observable socket object`, () => {
       const server = { test: 'test' };
-      const result = ServerAndEventStreamsFactory.create(server);
+      const result = ServerAndEventStreamsFactory.create(
+        new NoopAdapter(),
+        server,
+      );
 
       expect(result).to.have.keys('init', 'connection', 'disconnect', 'server');
       expect(result.init instanceof ReplaySubject).to.be.true;
       expect(result.connection instanceof Subject).to.be.true;
-      expect(result.disconnect instanceof Subject).to.be.true;
       expect(result.server).to.be.eql(server);
     });
   });
