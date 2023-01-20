9de14aa6f810b50aceca7b317bda1850930219d0
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-01-19 20:29:34 -0400

    fix(common): streamable file for headers already sent case

--- packages/common/file-stream/streamable-file.ts
+++ packages/common/file-stream/streamable-file.ts
@@ -4,9 +4,16 @@ import { isFunction } from '../utils/shared.utils';
 import { StreamableFileOptions } from './streamable-options.interface';
 
 export interface StreamableHandlerResponse {
+  /** `true` if the connection is destroyed, `false` otherwise. */
   destroyed: boolean;
+  /** `true` if headers were sent, `false` otherwise. */
+  headersSent: boolean;
+  /** The status code that will be sent to the client when the headers get flushed. */
   statusCode: number;
-  send: (msg: string) => void;
+  /** Sends the HTTP response. */
+  send: (body: string) => void;
+  /** Signals to the server that all of the response headers and body have been sent. */
+  end: () => void;
 }
 
 export class StreamableFile {
@@ -16,10 +23,16 @@ export class StreamableFile {
     err: Error,
     response: StreamableHandlerResponse,
   ) => void = (err: Error, res) => {
-    if (!res.destroyed) {
-      res.statusCode = 400;
-      res.send(err.message);
+    if (res.destroyed) {
+      return;
     }
+    if (res.headersSent) {
+      res.end();
+      return;
+    }
+
+    res.statusCode = 400;
+    res.send(err.message);
   };
 
   constructor(buffer: Uint8Array, options?: StreamableFileOptions);
