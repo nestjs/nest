2c3620aa1e9ebb90e4a58b9098dd332cf5d794e1
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2023-02-01 10:32:48 +0100

    chore: remove spec files from the integration dir, fix eslint errors

--- packages/core/inspector/noop-graph-inspector.ts
+++ packages/core/inspector/noop-graph-inspector.ts
@@ -1,5 +1,6 @@
 import { GraphInspector } from './graph-inspector';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {};
 export const NoopGraphInspector: GraphInspector = new Proxy(
   GraphInspector.prototype,
