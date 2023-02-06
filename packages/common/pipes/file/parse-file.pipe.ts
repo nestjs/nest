57d8ab9a23cd92c2450fdaedcda55efe65ab5473
Mohammad Reza Momeni <m.momeni@tejaratshayan.com>
2023-02-06 13:15:05 +0330

    refactor: correct grammer of variable

--- packages/common/pipes/file/parse-file.pipe.ts
+++ packages/common/pipes/file/parse-file.pipe.ts
@@ -39,12 +39,12 @@ export class ParseFilePipe implements PipeTransform<any> {
   }
 
   async transform(value: any): Promise<any> {
-    const areThereNoFileIn = this.thereAreNoFilesIn(value);
+    const areThereAnyFileIn = this.thereAreNoFilesIn(value);
 
-    if (areThereNoFileIn && this.fileIsRequired) {
+    if (areThereAnyFileIn && this.fileIsRequired) {
       throw this.exceptionFactory('File is required');
     }
-    if (!areThereNoFileIn && this.validators.length) {
+    if (!areThereAnyFileIn && this.validators.length) {
       await this.validateFilesOrFile(value);
     }
 
