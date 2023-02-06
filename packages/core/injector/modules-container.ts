d323edbab9b1f92bb4ef661bb5dd5caee2cce553
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2023-02-06 10:33:39 +0100

    chore(core): remove uuid dependency

--- packages/core/injector/modules-container.ts
+++ packages/core/injector/modules-container.ts
@@ -1,8 +1,8 @@
-import { v4 as uuid } from 'uuid';
+import { uid } from 'uid';
 import { Module } from './module';
 
 export class ModulesContainer extends Map<string, Module> {
-  private readonly _applicationId = uuid();
+  private readonly _applicationId = uid(21);
 
   get applicationId(): string {
     return this._applicationId;
--- packages/core/inspector/deterministic-uuid-registry.ts
+++ packages/core/inspector/deterministic-uuid-registry.ts
@@ -1,12 +1,13 @@
-import { v5 as uuid } from 'uuid';
-
+import { xxh32 } from '@node-rs/xxhash';
 const DEFAULT_UUID_NAMESPACE = 'efa0df42-88af-474f-9cad-4206a2319f07';
 
 export class DeterministicUuidRegistry {
-  private static readonly registry = new Set<string>();
+  private static readonly registry = new Set<number>();
 
   static get(str: string, namespace: string = DEFAULT_UUID_NAMESPACE, inc = 0) {
-    const id = inc ? uuid(str + `${inc}`, namespace) : uuid(str, namespace);
+    const id = inc
+      ? xxh32(`${namespace}_${str}_${inc}`)
+      : xxh32(`${namespace}_${str}`);
     if (this.registry.has(id)) {
       return this.get(str, namespace, inc + 1);
     }
