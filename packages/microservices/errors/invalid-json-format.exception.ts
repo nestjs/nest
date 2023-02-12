b1398ec4c2230736f4af297ce4da70a6dd037a03
Tony133 <a.tripodi133@gmail.com>
2023-02-12 11:25:06 +0100

    chore(microservices): mark facing apis public

--- packages/microservices/errors/invalid-json-format.exception.ts
+++ packages/microservices/errors/invalid-json-format.exception.ts
@@ -1,3 +1,6 @@
+/**
+ * @publicApi
+ */
 export class InvalidJSONFormatException extends Error {
   constructor(err: Error, data: string) {
     super(`Could not parse JSON: ${err.message}\nRequest data: ${data}`);
