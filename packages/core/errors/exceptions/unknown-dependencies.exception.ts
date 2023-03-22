36e8bfca81108025f0ab2ee40a43876472eae585
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2023-03-22 09:46:33 +0100

    fix(core): circular structure error (jest) #11303

--- packages/core/errors/exceptions/unknown-dependencies.exception.ts
+++ packages/core/errors/exceptions/unknown-dependencies.exception.ts
@@ -4,12 +4,15 @@ import { UNKNOWN_DEPENDENCIES_MESSAGE } from '../messages';
 import { RuntimeException } from './runtime.exception';
 
 export class UnknownDependenciesException extends RuntimeException {
+  public readonly moduleRef: { id: string } | undefined;
+
   constructor(
     public readonly type: string | symbol,
     public readonly context: InjectorDependencyContext,
-    public readonly moduleRef?: Module,
+    moduleRef?: Module,
     public readonly metadata?: { id: string },
   ) {
     super(UNKNOWN_DEPENDENCIES_MESSAGE(type, context, moduleRef));
+    this.moduleRef = moduleRef && { id: moduleRef.id };
   }
 }
