4d71fe41521f752502a1f2327e1372dd14636f79
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2023-03-29 14:39:23 +0200

    sample(19-auth): update sample to not use passport

--- sample/19-auth-jwt/src/auth/guards/local-auth.guard.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
--- sample/19-auth-jwt/src/users/users.service.spec.ts
+++ sample/19-auth-jwt/src/users/users.service.spec.ts
@@ -1,27 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { User, UsersService } from './users.service';
+import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
-    const moduleRef: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
     }).compile();
 
-    service = moduleRef.get<UsersService>(UsersService);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it.each`
-    name      | returnVal
-    ${'john'} | ${{ userId: 1, username: 'john', password: 'changeme' }}
-  `(
-    'should call findOne for $name and return $returnVal',
-    async ({ name, returnVal }: { name: string; returnVal: User }) => {
-      expect(await service.findOne(name)).toEqual(returnVal);
-    },
-  );
 });
