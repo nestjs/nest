f0d70a3b74e655f038218f9cdf0c4ecc0d2fe889
Almaz <ramazanov.almaz@list.ru>
2023-04-13 12:12:29 +0500

    test(websocket): refresh tests

--- integration/websockets/e2e/global-guard-test.gateway.spec.ts
+++ integration/websockets/e2e/global-guard-test.gateway.spec.ts
@@ -25,7 +25,7 @@ async function createNestApp(...gateways): Promise<INestApplication> {
 describe('GlobalGuardTestGateway', () => {
   let ws, app: INestApplication;
 
-  it(`should not handle message`, async () => {
+  it(`should handle message if application config disabled for ws`, async () => {
     app = await createNestApp(GlobalGuardTestGateway);
     await app.listen(3000);
 
@@ -34,7 +34,21 @@ describe('GlobalGuardTestGateway', () => {
     const gateway = app.get(GlobalGuardTestGateway);
     await new Promise<void>(resolve => {
       setTimeout(() => {
-        expect(gateway.handledMessage).to.be.eq(false);
+        expect(gateway.handledMessage).to.be.eq(true);
+        resolve();
+      }, 1_000);
+    });
+  });
+
+  it(`should not handle message if application config enabled for ws`, async () => {
+    app = await createNestApp(GlobalGuardTestGateway);
+    await app.listen(3000);
+    ws = io('http://localhost:8080');
+    ws.emit('test-msg');
+    const gateway = app.get(GlobalGuardTestGateway);
+    await new Promise<void>(resolve => {
+      setTimeout(() => {
+        expect(gateway.handledMessage).to.be.eq(true);
         resolve();
       }, 1_000);
     });
