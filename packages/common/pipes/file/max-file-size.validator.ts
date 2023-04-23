84bd87c86ad4d02ae70a8cdffbd04c7b96b8b4b4
Anjorin Damilare <damilareanjorin1@gmail.com>
2023-04-23 06:57:52 +0100

    chore: add ability to pass custom message in file size validator

--- packages/common/pipes/file/max-file-size.validator.ts
+++ packages/common/pipes/file/max-file-size.validator.ts
@@ -2,6 +2,7 @@ import { FileValidator } from './file-validator.interface';
 
 export type MaxFileSizeValidatorOptions = {
   maxSize: number;
+  message?: string | (file?: any) => string;
 };
 
 /**
@@ -12,7 +13,15 @@ export type MaxFileSizeValidatorOptions = {
  * @publicApi
  */
 export class MaxFileSizeValidator extends FileValidator<MaxFileSizeValidatorOptions> {
-  buildErrorMessage(): string {
+  buildErrorMessage(file: any): string {
+    if ('message' in this.validationOptions) {
+      if (typeof this.validationOptions.message === 'function') {
+        return this.validationOptions.message(file);
+      }
+
+      return this.validationOptions.message;
+    }
+
     return `Validation failed (expected size is less than ${this.validationOptions.maxSize})`;
   }
 
