d5d9fc28d6b34c885f63e8fcd8f55b65383b8912
Matteias Collet <matteias.collet@bluewin.ch>
2023-04-25 11:03:11 +0000

    feat(common): add too many requests exception class

---
+++ packages/common/exceptions/too-many-requests.exception.ts
@@ -0,0 +1,53 @@
+import { HttpStatus } from '../enums/http-status.enum';
+import { HttpException, HttpExceptionOptions } from './http.exception';
+
+/**
+ * Defines an HTTP exception for *Too Many Requests* type errors.
+ *
+ * @see [Built-in HTTP exceptions](https://docs.nestjs.com/exception-filters#built-in-http-exceptions)
+ *
+ * @publicApi
+ */
+export class TooManyRequestsException extends HttpException {
+  /**
+   * Instantiate a `TooManyRequestsException` Exception.
+   *
+   * @example
+   * `throw new TooManyRequestsException()`
+   *
+   * @usageNotes
+   * The HTTP response status code will be 429.
+   * - The `objectOrError` argument defines the JSON response body or the message string.
+   * - The `descriptionOrOptions` argument contains either a short description of the HTTP error or an options object used to provide an underlying error cause.
+   *
+   * By default, the JSON response body contains two properties:
+   * - `statusCode`: this will be the value 429.
+   * - `message`: the string `'Too Many Request'` by default; override this by supplying
+   * a string in the `objectOrError` parameter.
+   *
+   * If the parameter `objectOrError` is a string, the response body will contain an
+   * additional property, `error`, with a short description of the HTTP error. To override the
+   * entire JSON response body, pass an object instead. Nest will serialize the object
+   * and return it as the JSON response body.
+   *
+   * @param objectOrError string or object describing the error condition.
+   * @param descriptionOrOptions either a short description of the HTTP error or an options object used to provide an underlying error cause
+   */
+  constructor(
+    objectOrError?: string | object | any,
+    descriptionOrOptions: string | HttpExceptionOptions = 'Too Many Requests',
+  ) {
+    const { description, httpExceptionOptions } =
+      HttpException.extractDescriptionAndOptionsFrom(descriptionOrOptions);
+
+    super(
+      HttpException.createBody(
+        objectOrError,
+        description,
+        HttpStatus.TOO_MANY_REQUESTS,
+      ),
+      HttpStatus.TOO_MANY_REQUESTS,
+      httpExceptionOptions,
+    );
+  }
+}
