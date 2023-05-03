66a74e0b3dc7c5a8505d3770f389ce8e5fdccdfe
Adrian Huminiuc <adrian.huminiuc@montonio.com>
2023-05-03 16:33:06 +0300

    fix(samples): apply logger middlware to routes

--- sample/10-fastify/src/app.module.ts
+++ sample/10-fastify/src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
+import {LoggerMiddleware} from "./common/middleware/logger.middleware";
 
 @Module({
   imports: [CatsModule, CoreModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
