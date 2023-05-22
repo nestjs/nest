49d853a1a81162e47a0c598d36be3ec0dcf600d5
9w <qwer9w@gmail.com>
2023-05-22 18:31:48 +0900

    fix(sample): change mysql connection host to 127.0.0.1

--- sample/05-sql-typeorm/src/app.module.ts
+++ sample/05-sql-typeorm/src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: 'root',
