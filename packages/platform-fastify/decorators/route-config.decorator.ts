ca4a48eec58660ee0e564288b5d4e3afb0903e45
Richard Lee <leerw4@gmail.com>
2023-08-10 20:32:36 -0400

    feat(fastify): adding jsdoc to route config decorator

--- packages/platform-fastify/decorators/route-config.decorator.ts
+++ packages/platform-fastify/decorators/route-config.decorator.ts
@@ -1,5 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 import { FASTIFY_ROUTE_CONFIG_METADATA } from '../constants';
 
+/**
+ * @param config See {@link https://fastify.dev/docs/latest/Reference/Routes/#config}
+ */
 export const RouteConfig = (config: any) =>
   SetMetadata(FASTIFY_ROUTE_CONFIG_METADATA, config);
