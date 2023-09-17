1a0c304767c31bebbf5dd1ef667b8c5737c3bc18
Tony133 <a.tripodi133@gmail.com>
2023-09-17 12:03:20 +0200

    chore(platform-fastify): marked the api public in route config decorator

--- packages/platform-fastify/decorators/route-config.decorator.ts
+++ packages/platform-fastify/decorators/route-config.decorator.ts
@@ -2,6 +2,8 @@ import { SetMetadata } from '@nestjs/common';
 import { FASTIFY_ROUTE_CONFIG_METADATA } from '../constants';
 
 /**
+ * @publicApi
+ * 
  * @param config See {@link https://fastify.dev/docs/latest/Reference/Routes/#config}
  */
 export const RouteConfig = (config: any) =>
