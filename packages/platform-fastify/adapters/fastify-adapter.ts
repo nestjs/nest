71f3f42501290c47fee04cf90c2ddc067c1a3c3b
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-11-01 01:32:14 +0330

    fix: Resolve type error

--- packages/platform-fastify/adapters/fastify-adapter.ts
+++ packages/platform-fastify/adapters/fastify-adapter.ts
@@ -101,8 +101,7 @@ type FastifyRawRequest<TServer extends RawServerBase> =
 export class FastifyAdapter<
   TServer extends RawServerBase = RawServerDefault,
   TRawRequest extends FastifyRawRequest<TServer> = FastifyRawRequest<TServer>,
-  TRawResponse extends
-    RawReplyDefaultExpression<TServer> = RawReplyDefaultExpression<TServer>,
+  TRawResponse extends RawReplyDefaultExpression<TServer> = RawReplyDefaultExpression<TServer>,
   TRequest extends FastifyRequest<
     RequestGenericInterface,
     TServer,
@@ -439,7 +438,7 @@ export class FastifyAdapter<
     return this.register(
       loadPackage('@fastify/static', 'FastifyAdapter.useStaticAssets()', () =>
         require('@fastify/static'),
-      ),
+      ) as unknown as Parameters<TInstance['register']>[0],
       options,
     );
   }
@@ -454,7 +453,7 @@ export class FastifyAdapter<
     return this.register(
       loadPackage('@fastify/view', 'FastifyAdapter.setViewEngine()', () =>
         require('@fastify/view'),
-      ),
+      ) as unknown as Parameters<TInstance['register']>[0],
       options,
     );
   }
