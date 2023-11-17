ae03ccc44f49ab843911d8688ec863a8eab592ac
Vinícius Lourenço <contact@viniciusl.com.br>
2023-11-17 10:54:28 -0300

    fixup! perf: prefer use factory instead of use value when possible

--- packages/core/test/router/router-module.spec.ts
+++ packages/core/test/router/router-module.spec.ts
@@ -16,12 +16,15 @@ describe('RouterModule', () => {
 
   describe('register', () => {
     it('should return a dynamic module with routes registered as a provider', () => {
-      const moduleRegistered = RouterModule.register(routes);
-      const provider = moduleRegistered.providers.find(
-        p => 'useFactory' in p && p.provide === ROUTES,
-      ) as FactoryProvider;
-      expect(provider).to.not.be.undefined;
-      expect(provider.useFactory()).to.be.eq(routes);
+      expect(RouterModule.register(routes)).to.deep.equal({
+        module: RouterModule,
+        providers: [
+          {
+            provide: ROUTES,
+            useValue: routes,
+          },
+        ],
+      });
     });
   });
   describe('when instantiated', () => {
