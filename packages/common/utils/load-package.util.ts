1ec7a93d394e9bf9dea2c811f1b52ffc6fa2b255
Pooyan Khanjankhani <pooyankhan@gmail.com>
2024-01-01 18:58:52 +0330

    fix: Persist ts-ignore comments after compile

--- packages/common/utils/load-package.util.ts
+++ packages/common/utils/load-package.util.ts
@@ -7,43 +7,43 @@ const logger = new Logger('PackageLoader');
 
 type KnownPackages = {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   ws: typeof import('ws');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   mqtt: typeof import('mqtt');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   nats: typeof import('nats');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   amqplib: typeof import('amqplib');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   ioredis: typeof import('ioredis');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   kafkajs: typeof import('kafkajs');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   '@grpc/grpc-js': typeof import('@grpc/grpc-js');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   '@fastify/view': typeof import('@fastify/view');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   '@fastify/static': typeof import('@fastify/static');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   'class-validator': typeof import('class-validator');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   'reflect-metadata': typeof import('reflect-metadata');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   'class-transformer': typeof import('class-transformer');
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore:next-line
+  /** @ts-ignore:next-line */
   'amqp-connection-manager': typeof import('amqp-connection-manager');
 };
 
