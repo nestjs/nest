33aae84e53a6aaba66dc5f719c15d0a5c9cebcda
Doron Guttman <doronguttman@users.noreply.github.com>
2024-01-31 14:01:38 -0500

    refactor(common): ♻️ PR #13000 comments
    https://github.com/nestjs/nest/pull/13000
    https://github.com/nestjs/nest/issues/12998

--- packages/core/helpers/router-method-factory.ts
+++ packages/core/helpers/router-method-factory.ts
@@ -1,12 +1,7 @@
 import { HttpServer } from '@nestjs/common';
 import { RequestMethod } from '@nestjs/common/enums/request-method.enum';
 
-/**
- * Ensures (via satisfies) there's a mapping between the RequestMethod enum and the HttpServer interface.
- * @note This is a compile-time check, so if the interface changes, the compiler will complain.
- * This is to resolve a case where a new RequestMethod is added, but the RouterMethodFactory is not updated.
- */
-const RequestMethodMap = {
+const REQUEST_METHOD_MAP = {
   [RequestMethod.GET]: 'get',
   [RequestMethod.POST]: 'post',
   [RequestMethod.PUT]: 'put',
@@ -20,10 +15,9 @@ const RequestMethodMap = {
 
 export class RouterMethodFactory {
   public get(target: HttpServer, requestMethod: RequestMethod): Function {
-    const methodName = RequestMethodMap[requestMethod];
+    const methodName = REQUEST_METHOD_MAP[requestMethod];
     const method = target[methodName];
     if (!method) {
-      // There should probably be a warning message in this case
       return target.use;
     }
     return method;
