af8fe730fff86c62fe7001968cebc60ab341fb76
YoungKi Lyu <youngkiu@gmail.com>
2024-03-27 23:40:14 +0900

    sample(04-grpc): Update codes reviewed by @ssilve1989

--- sample/04-grpc/server/src/helloworld/helloworld.controller.ts
+++ sample/04-grpc/server/src/helloworld/helloworld.controller.ts
@@ -1,52 +1,41 @@
 // https://github.com/grpc/grpc-node/blob/master/examples/error_handling/server.js
 import { Controller } from '@nestjs/common';
 import { GrpcMethod, RpcException } from '@nestjs/microservices';
-import { Observable, ReplaySubject } from 'rxjs';
+import { Observable, of, repeat } from 'rxjs';
 import {
   GreeterController,
   HelloReply,
   HelloRequest,
 } from '../interface/helloworld';
-import * as grpc from '@grpc/grpc-js';
+import { status } from '@grpc/grpc-js';
 
 @Controller()
 export class HelloworldController implements GreeterController {
   private readonly REPLY_COUNT = 5;
 
   @GrpcMethod('Greeter', 'sayHello')
-  sayHello(request: HelloRequest): HelloReply {
-    if (request.name === '') {
+  sayHello({ name }: HelloRequest): HelloReply {
+    if (!name) {
       throw new RpcException({
-        code: grpc.status.INVALID_ARGUMENT,
+        code: status.INVALID_ARGUMENT,
         message: 'request missing required field: name',
       });
     }
 
     return {
-      message: 'Hello ' + request.name,
+      message: 'Hello ' + name,
     };
   }
 
   @GrpcMethod('Greeter', 'sayHelloStreamReply')
-  sayHelloStreamReply(request: HelloRequest): Observable<HelloReply> {
-    const hero$ = new ReplaySubject<HelloReply>();
-
-    if (request.name === '') {
-      hero$.error(
-        new RpcException({
-          code: grpc.status.INVALID_ARGUMENT,
-          message: 'request missing required field: name',
-        }),
-      );
-
-      return hero$.asObservable();
+  sayHelloStreamReply({ name }: HelloRequest): Observable<HelloReply> {
+    if (!name) {
+      throw new RpcException({
+        code: status.INVALID_ARGUMENT,
+        message: 'request missing required field: name',
+      });
     }
 
-    for (let i = 0; i < this.REPLY_COUNT; i++) {
-      hero$.next({ message: 'Hello ' + request.name });
-    }
-    hero$.complete();
-
-    return hero$.asObservable();
+    return of({ message: `Hello ${name}` }).pipe(repeat(this.REPLY_COUNT));
   }
 }
