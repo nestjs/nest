7d5adfcc1faf8a103933e488d104aba5ccb9a26d
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2024-04-19 09:14:09 +0200

    Revert "Revert "fix(microservice) Delete unnecessary call of grpcClient.start""

--- packages/microservices/server/server-grpc.ts
+++ packages/microservices/server/server-grpc.ts
@@ -81,7 +81,6 @@ export class ServerGrpc extends Server implements CustomTransportStrategy {
 
   public async start(callback?: () => void) {
     await this.bindEvents();
-    this.grpcClient.start();
     callback();
   }
 
