365fd1e5d8ce6cfdae7ce2cf7abec67aca88fc92
Johaven <johan@sync-in.com>
2024-07-22 01:18:59 +0200

    fix: typing

--- packages/platform-fastify/adapters/fastify-adapter.ts
+++ packages/platform-fastify/adapters/fastify-adapter.ts
@@ -34,6 +34,7 @@ import {
   RequestGenericInterface,
   RouteOptions,
   RouteShorthandOptions,
+  HTTPMethods,
   fastify,
 } from 'fastify';
 import * as Reply from 'fastify/lib/reply';
@@ -672,17 +673,7 @@ export class FastifyAdapter<
     return rawRequest.originalUrl || rawRequest.url;
   }
 
-  private injectRouteOptions(
-    routerMethodKey:
-      | 'get'
-      | 'post'
-      | 'put'
-      | 'delete'
-      | 'options'
-      | 'patch'
-      | 'head',
-    ...args: any[]
-  ) {
+  private injectRouteOptions(routerMethodKey: HTTPMethods, ...args: any[]) {
     const handlerRef = args[args.length - 1];
     const isVersioned =
       !isUndefined(handlerRef.version) &&
