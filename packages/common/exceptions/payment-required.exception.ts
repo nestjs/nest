efd35b4fbcd9dd5d50d9cc8b29b4de17186e30ba
Tawfek Daghistani <tawfekov@gmail.com>
2024-07-28 13:38:51 +0300

    add payment required exception

---
+++ packages/common/exceptions/payment-required.exception.ts
@@ -0,0 +1,49 @@
+import { HttpStatus } from '../enums/http-status.enum';
+import { HttpException, HttpExceptionOptions } from './http.exception';
+
+/**
+ * Defines an HTTP exception for *Payment required exception* type errors.
+ *
+ * @see [Built-in HTTP exceptions](https://docs.nestjs.com/exception-filters#built-in-http-exceptions)
+ *
+ * @publicApi
+ */
+export class PaymentRequiredException extends HttpException {
+  /**
+   * Instantiate a `PaymentRequiredException` Exception.
+   *
+   * @example
+   * `throw new PaymentRequiredException()`
+   *
+   * @usageNotes
+   * The HTTP response status code will be 402.
+   * - The `objectOrError` argument defines the JSON response body or the message string.
+   * - The `descriptionOrOptions` argument contains either a short description of the HTTP error or an options object used to provide an underlying error cause.
+   *
+   * By default, the JSON response body contains two properties:
+   * - `statusCode`: this will be the value 402.
+   * - `message`: the string `'PaymentRequiredException'` by default; override this by supplying
+   * a string in the `objectOrError` parameter.
+   *
+   * If the parameter `objectOrError` is a string, the response body will contain an
+   * additional property, `error`, with a short description of the HTTP error. To override the
+   * entire JSON response body, pass an object instead. Nest will serialize the object
+   * and return it as the JSON response body.
+   *
+   * @param objectOrError string or object describing the error condition.
+   * @param descriptionOrOptions either a short description of the HTTP error or an options object used to provide an underlying error cause
+   */
+  constructor(
+    objectOrError?: string | object | any,
+    descriptionOrOptions: string | HttpExceptionOptions = 'PaymentRequiredException',
+  ) {
+    const { description, httpExceptionOptions } =
+      HttpException.extractDescriptionAndOptionsFrom(descriptionOrOptions);
+
+    super(
+      HttpException.createBody(objectOrError, description, HttpStatus.PaymentRequiredException),
+      HttpStatus.PaymentRequiredException,
+      httpExceptionOptions,
+    );
+  }
+}
