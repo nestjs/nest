ebae5a1bb2996d799cdc008b989fdf90fcb1f8af
Adib Grouz <contact@adib-grouz.com>
2024-09-01 12:59:19 +0200

    feat(common): add new enums to common/enums

---
+++ packages/common/enums/cors-setting.enum.ts
@@ -0,0 +1,8 @@
+export enum CORSSetting {
+  ALLOW_ORIGIN = 'Access-Control-Allow-Origin',
+  ALLOW_METHODS = 'Access-Control-Allow-Methods',
+  ALLOW_HEADERS = 'Access-Control-Allow-Headers',
+  EXPOSE_HEADERS = 'Access-Control-Expose-Headers',
+  MAX_AGE = 'Access-Control-Max-Age',
+  ALLOW_CREDENTIALS = 'Access-Control-Allow-Credentials',
+}
