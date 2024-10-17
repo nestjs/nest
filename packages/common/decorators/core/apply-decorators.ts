e6ed2f37a6e931e00ba7f9dba5ef75e7331e03b3
nikelborm <29357907+nikelborm@users.noreply.github.com>
2024-10-17 16:30:28 +0300

    fix: better apply-decorators and better tests for it

--- packages/common/decorators/core/apply-decorators.ts
+++ packages/common/decorators/core/apply-decorators.ts
@@ -1,3 +1,6 @@
+// ClassDecorator, MethodDecorator, PropertyDecorator, ParameterDecorator defined here:
+// https://github.com/microsoft/TypeScript/blob/main/src/lib/decorators.legacy.d.ts
+
 /**
  * Function that returns a new decorator that applies all decorators provided by param
  *
@@ -8,35 +11,125 @@
  * @publicApi
  */
 export function applyDecorators(
-  ...decorators: Array<ClassDecorator | MethodDecorator | PropertyDecorator>
-) {
+  ...decoratorsToCall: Array<ClassDecorator>
+): ClassDecorator;
+export function applyDecorators(
+  ...decoratorsToCall: Array<MethodDecorator>
+): MethodDecorator;
+export function applyDecorators(
+  ...decoratorsToCall: Array<PropertyDecorator>
+): PropertyDecorator;
+export function applyDecorators(
+  ...decoratorsToCall: Array<ParameterDecorator>
+): ParameterDecorator;
+// applyDecorators<T> for cases when decorator has complex signature like `UseGuards(...): ClassDecorator & MethodDecorator`
+export function applyDecorators<T>(...decoratorsToCall: Array<T>): T;
+export function applyDecorators(
+  ...decoratorsToCall: Array<
+    ClassDecorator | MethodDecorator | PropertyDecorator | ParameterDecorator
+  >
+): ClassDecorator | MethodDecorator | PropertyDecorator | ParameterDecorator {
   return <TFunction extends Function, Y>(
-    target: TFunction | object,
+    target: TFunction | Object,
     propertyKey?: string | symbol,
-    descriptor?: TypedPropertyDescriptor<Y>,
-  ): void => {
-    for (const decorator of decorators) {
-      if (target instanceof Function && descriptor === undefined && propertyKey === undefined) {
-        (decorator as ClassDecorator)(target);
-        continue;
+    descriptorOrParameterIndex?: number | TypedPropertyDescriptor<Y>,
+  ) => {
+    const whatIsBeingDecorated =
+      target instanceof Function &&
+      propertyKey === undefined &&
+      descriptorOrParameterIndex === undefined
+        ? 'Class'
+        : target instanceof Object &&
+            ['symbol', 'string'].includes(typeof propertyKey) &&
+            descriptorOrParameterIndex === undefined
+          ? 'Property'
+          : target instanceof Object &&
+              ['symbol', 'string'].includes(typeof propertyKey) &&
+              descriptorOrParameterIndex instanceof Object
+            ? 'Method'
+            : target instanceof Object &&
+                ['symbol', 'string', 'undefined'].includes(
+                  typeof propertyKey,
+                ) &&
+                typeof descriptorOrParameterIndex === 'number'
+              ? 'Parameter'
+              : 'Unknown';
+
+    const typedDecoratorsWithTheirTypedArgs = {
+      Class: {
+        decorators: decoratorsToCall as ClassDecorator[],
+        args: [target] as [TFunction],
+      },
+      Property: {
+        decorators: decoratorsToCall as PropertyDecorator[],
+        args: [target, propertyKey] as [Object, string | symbol],
+      },
+      Method: {
+        decorators: decoratorsToCall as MethodDecorator[],
+        args: [target, propertyKey, descriptorOrParameterIndex] as [
+          Object,
+          string | symbol,
+          TypedPropertyDescriptor<Y>,
+        ],
+      },
+      Parameter: {
+        decorators: decoratorsToCall as ParameterDecorator[],
+        args: [target, propertyKey, descriptorOrParameterIndex] as [
+          Object,
+          string | symbol | undefined,
+          number,
+        ],
+      },
+    } as const;
+
+    if (whatIsBeingDecorated === 'Class') {
+      const { decorators, args } =
+        typedDecoratorsWithTheirTypedArgs[whatIsBeingDecorated];
+
+      let sequentiallyExtendedClass: TFunction | void = undefined;
+
+      for (const decorator of decorators) {
+        if (sequentiallyExtendedClass) {
+          sequentiallyExtendedClass =
+            decorator(sequentiallyExtendedClass) ?? sequentiallyExtendedClass;
+        } else {
+          sequentiallyExtendedClass = decorator(...args) ?? args[0];
+        }
       }
 
-      if (propertyKey === undefined) {
-        throw new Error('Parameter decorators are not supported in applyDecorators function');
+      return sequentiallyExtendedClass;
+    } else if (whatIsBeingDecorated === 'Method') {
+      const { decorators, args } =
+        typedDecoratorsWithTheirTypedArgs[whatIsBeingDecorated];
+      let sequentiallyExtendedDescriptor: TypedPropertyDescriptor<Y> | void =
+        undefined;
+
+      for (const decorator of decorators) {
+        if (sequentiallyExtendedDescriptor) {
+          sequentiallyExtendedDescriptor =
+            decorator(args[0], args[1], sequentiallyExtendedDescriptor) ??
+            sequentiallyExtendedDescriptor;
+        } else {
+          sequentiallyExtendedDescriptor = decorator(...args) ?? args[2];
+        }
       }
 
-      if (descriptor !== undefined) {
-        (decorator as MethodDecorator)(
-          target,
-          propertyKey,
-          descriptor,
-        );
-        continue;
+      return sequentiallyExtendedDescriptor;
+    } else if (whatIsBeingDecorated === 'Parameter') {
+      const { decorators, args } =
+        typedDecoratorsWithTheirTypedArgs[whatIsBeingDecorated];
+      for (const decorator of decorators) {
+        decorator(...args);
       }
-
-      (decorator as PropertyDecorator)(
-        target,
-        propertyKey,
+    } else if (whatIsBeingDecorated === 'Property') {
+      const { decorators, args } =
+        typedDecoratorsWithTheirTypedArgs[whatIsBeingDecorated];
+      for (const decorator of decorators) {
+        decorator(...args);
+      }
+    } else {
+      throw new Error(
+        'Decorator created by applyDecorators was called with invalid arguments. It can only be used on a class, method, property or parameter.',
       );
     }
   };
