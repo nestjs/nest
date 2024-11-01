ba23f5e64cf7fadb3dbf5919cfb727831ff28633
Dushmanta <dushmanta0511@gmail.com>
2024-11-01 16:29:29 +0530

    sample(fastify): update deprecated variadic argument usage in app.listen() method

--- sample/10-fastify/src/main.ts
+++ sample/10-fastify/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen({ port: 3000 });
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
