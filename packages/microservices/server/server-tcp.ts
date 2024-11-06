9cb84e78f8ce34f5ad75f63e3e941abb087fe594
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2024-11-06 12:47:19 +0100

    Update packages/microservices/server/server-tcp.ts
    Co-authored-by: Gil Tichon <gil.tichon@lemonade.com>

--- packages/microservices/server/server-tcp.ts
+++ packages/microservices/server/server-tcp.ts
@@ -41,9 +41,9 @@ export class ServerTCP extends Server implements CustomTransportStrategy {
   constructor(private readonly options: TcpOptions['options']) {
     super();
     this.port = this.getOptionsProp(options, 'port', TCP_DEFAULT_PORT);
-    this.host = this.getOptionsProp(options, 'host') || TCP_DEFAULT_HOST;
+    this.host = this.getOptionsProp(options, 'host', TCP_DEFAULT_HOST);
     this.socketClass =
-      this.getOptionsProp(options, 'socketClass') || JsonSocket;
+      this.getOptionsProp(options, 'socketClass', JsonSocket);
     this.tlsOptions = this.getOptionsProp(options, 'tlsOptions');
 
     this.init();
