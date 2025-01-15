e60ba34190901786b99df6b5d68e6ec52dd2be55
Bartosz Majkut <bmajkut91@gmail.com>
2025-01-16 00:21:42 +0100

    fix(sample): Add missing query to posts sample schema and add missing post field in interface.

--- sample/32-graphql-federation-schema-first/posts-application/src/posts/posts.interfaces.ts
+++ sample/32-graphql-federation-schema-first/posts-application/src/posts/posts.interfaces.ts
@@ -4,4 +4,5 @@ export interface Post {
   id: number;
   title: string;
   authorId: number;
+  body: string;
 }
--- sample/32-graphql-federation-schema-first/posts-application/src/posts/posts.service.ts
+++ sample/32-graphql-federation-schema-first/posts-application/src/posts/posts.service.ts
@@ -4,10 +4,10 @@ import { Post } from './posts.interfaces';
 @Injectable()
 export class PostsService {
   private posts: Post[] = [
-    { authorId: 1, id: 1, title: 'Lorem Ipsum' },
-    { authorId: 1, id: 2, title: 'Foo' },
-    { authorId: 2, id: 3, title: 'Bar' },
-    { authorId: 2, id: 4, title: 'Hello World' },
+    { authorId: 1, id: 1, title: 'Lorem Ipsum', body: 'Foo' },
+    { authorId: 1, id: 2, title: 'Foo', body: 'Bar' },
+    { authorId: 2, id: 3, title: 'Bar', body: 'Baz' },
+    { authorId: 2, id: 4, title: 'Hello World', body: 'World' },
   ];
 
   findOneByAuthorId(authorId: number) {
