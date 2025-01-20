d03955dfda9a9807d45edd6b0887abcce69ff759
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-01-20 12:34:27 +0100

    sample: update 24-serve-static sample

--- sample/24-serve-static/src/app.module.ts
+++ sample/24-serve-static/src/app.module.ts
@@ -7,7 +7,10 @@ import { AppController } from './app.controller';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api/(.*)'],
+      exclude: ['/api/{*test}'],
+      serveStaticOptions: {
+        fallthrough: false,
+      },
     }),
   ],
   controllers: [AppController],
