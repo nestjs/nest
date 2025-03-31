4d21f1e4914a252b23f8ee3b9ed940585490ffec
Chathula <schathula@gmail.com>
2025-03-31 15:12:28 +0200

    fix: fixed sample typo

--- sample/29-file-upload/src/app.controller.ts
+++ sample/29-file-upload/src/app.controller.ts
@@ -39,7 +39,7 @@ export class AppController {
     @Body() body: SampleDto,
     @UploadedFile(
       new ParseFilePipeBuilder()
-        .addypeValidator({
+        .addFileTypeValidator({
           fileType: 'json',
         })
         .build({
@@ -60,7 +60,7 @@ export class AppController {
     @Body() body: SampleDto,
     @UploadedFile(
       new ParseFilePipeBuilder()
-        .addFileTlidator({
+        .addFileTypeValidator({
           fileType: 'jpg',
         })
         .build(),
