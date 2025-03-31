7a2e5bab4403b573d7cdde3caeadce883c635d74
Chathula <schathula@gmail.com>
2025-03-31 13:05:46 +0200

    fix(common): introduce magic file type validator to nestjs common

--- packages/common/pipes/file/interfaces/file.interface.ts
+++ packages/common/pipes/file/interfaces/file.interface.ts
@@ -2,3 +2,11 @@ export interface IFile {
   mimetype: string;
   size: number;
 }
+
+export interface IFileWithBuffer extends IFile {
+  buffer: Buffer;
+}
+
+export type FileTypeValidatorOptions = {
+  fileType: string | RegExp;
+};
