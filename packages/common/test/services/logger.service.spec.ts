bb9e847b9e781e53ec99b3539220554f13a48d67
Julien Barbay <julien.barbay@gmail.com>
2025-04-17 12:53:49 +0700

    fix(common): fix chai assertions in logger service spec

--- packages/common/test/services/logger.service.spec.ts
+++ packages/common/test/services/logger.service.spec.ts
@@ -14,27 +14,27 @@ describe('Logger', () => {
     describe('when using filterLogLevels', () => {
       it('should correctly parse an exclusive range', () => {
         const returned = filterLogLevels('>warn');
-        expect(returned).toEqual(['error', 'fatal']);
+        expect(returned).to.deep.equal(['error', 'fatal']);
       });
 
       it('should correctly parse an inclusive range', () => {
         const returned = filterLogLevels('>=warn');
-        expect(returned).toEqual(['warn', 'error', 'fatal']);
+        expect(returned).to.deep.equal(['warn', 'error', 'fatal']);
       });
 
       it('should correctly parse a string list', () => {
         const returned = filterLogLevels('verbose,warn,fatal');
-        expect(returned).toEqual(['verbose', 'warn', 'fatal']);
+        expect(returned).to.deep.equal(['verbose', 'warn', 'fatal']);
       });
 
       it('should correctly parse a single log level', () => {
         const returned = filterLogLevels('debug');
-        expect(returned).toEqual(['debug']);
+        expect(returned).to.deep.equal(['debug']);
       });
 
       it('should return all otherwise', () => {
         const returned = filterLogLevels();
-        expect(returned).toEqual([
+        expect(returned).to.deep.equal([
           'verbose',
           'debug',
           'log',
