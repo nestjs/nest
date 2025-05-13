32565cf1a3d297707153e48376146ddc44f1ab82
Ozair Aziz <uzair.aziz34@gmail.com>
2025-05-13 15:09:46 +0300

    test(express): Enhance tests for enableCaseInsensitiveQueries
    - Add tests for arrays, nested objects, and empty queries.
    
    Closes #15115

--- packages/platform-express/test/adapters/express-adapter.spec.ts
+++ packages/platform-express/test/adapters/express-adapter.spec.ts
@@ -1,7 +1,29 @@
+import { Controller, Get, INestApplication, Query, Req } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
 import { expect } from 'chai';
 import * as express from 'express';
 import * as sinon from 'sinon';
+import { Request } from 'express';
+
+@Controller('test')
+export class AppController {
+  @Get()
+  getQuery(@Req() req: Request) {
+    const normalizedQuery = req.query;
+    const response = Object.entries(normalizedQuery)
+      .map(([key, value]) => {
+        const displayValue = Array.isArray(value)
+          ? value.join(', ')
+          : (value?.toString() ?? 'undefined');
+        return `${key}: ${displayValue}`;
+      })
+      .join(', ');
+    return response || 'No query parameters provided';
+  }
+}
 
 describe('ExpressAdapter', () => {
   afterEach(() => sinon.restore());
@@ -41,6 +63,52 @@ describe('ExpressAdapter', () => {
       expressAdapter.registerParserMiddleware();
 
       expect(useSpy.called).to.be.false;
+    });
+  });
+
+  describe('ExpressAdapter', () => {
+    let app: INestApplication & NestExpressApplication;
+
+    beforeEach(async () => {
+      const module = await Test.createTestingModule({
+        controllers: [AppController],
+      }).compile();
+      app = module.createNestApplication<NestExpressApplication>(
+        new ExpressAdapter(),
+        {},
+      );
+      app.enableCaseInsensitiveQueries();
+      await app.init();
+    });
+
+    afterEach(async () => {
+      await app.close();
+    });
+
+    it('normalizes all query parameters to lowercase', async () => {
+      await request(app.getHttpServer())
+        .get('/test?NAME=John&AGE=30&CiTy=NewYork&EMAIL=john@example.com')
+        .expect(
+          200,
+          'name: John, age: 30, city: NewYork, email: john@example.com',
+        );
+    });
+
+    it('handles empty queries', async () => {
+      await request(app.getHttpServer())
+        .get('/test')
+        .expect(200, 'No query parameters provided');
+    });
+
+    it('handles arrays and nested objects', async () => {
+      await request(app.getHttpServer())
+        .get(
+          '/test?NAMES=John,Jane&AGE=30&DETAILS[address]=123+St&DETAILS[city]=NY',
+        )
+        .expect(
+          200,
+          'names: John, Jane, age: 30, details[address]: 123 St, details[city]: NY',
+        );
     });
   });
 });
