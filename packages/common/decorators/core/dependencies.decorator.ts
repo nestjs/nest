2d63ebff73e8f0d0a0d8ba0e489ba3925e0dd99d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-07-15 12:02:36 +0200

    chore: upgrade typescript

--- packages/common/decorators/core/dependencies.decorator.ts
+++ packages/common/decorators/core/dependencies.decorator.ts
@@ -4,7 +4,9 @@ export function flatten<T extends Array<unknown> = any>(
   arr: T,
 ): T extends Array<infer R> ? R : never {
   const flat = ([] as any[]).concat(...arr);
-  return flat.some(Array.isArray) ? flatten(flat) : flat;
+  return flat.some(Array.isArray)
+    ? flatten(flat)
+    : (flat as T extends Array<infer R> ? R : never);
 }
 
 /**
