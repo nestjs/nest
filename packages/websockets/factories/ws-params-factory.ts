e9371344f6974566374e2d08a0012c0f0aaf739d
lifefloating <imshuazi@126.com>
2025-08-01 17:26:54 +0800

    feat(ws): Support for dynamic path params in websocket, add tests

--- packages/websockets/factories/ws-params-factory.ts
+++ packages/websockets/factories/ws-params-factory.ts
@@ -14,6 +14,21 @@ export class WsParamsFactory {
         return args[0];
       case WsParamtype.PAYLOAD:
         return data ? args[1]?.[data] : args[1];
+      case WsParamtype.PARAM: {
+        // Path parameters are extracted from the WebSocket handshake request
+        // and stored in the client object during connection establishment
+        const client = args[0] as any;
+        const pathParams =
+          client?._pathParams ||
+          client?.upgradeReq?.params ||
+          client?.request?.params;
+
+        if (!pathParams) {
+          return data ? undefined : {};
+        }
+
+        return data && pathParams ? pathParams[data] : pathParams;
+      }
       default:
         return null;
     }
