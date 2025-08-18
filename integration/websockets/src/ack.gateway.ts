0c7b94d34e4851c63aab0579bda007c380d0a435
Seongjee Kim <lsvk3577@naver.com>
2025-08-18 19:13:55 +0900

    feat(ws): add integration for manually ack

--- integration/websockets/src/ack.gateway.ts
+++ integration/websockets/src/ack.gateway.ts
@@ -1,9 +1,29 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  Ack,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 
 @WebSocketGateway(8080)
 export class AckGateway {
   @SubscribeMessage('push')
   onPush() {
     return 'pong';
+  }
+
+  @SubscribeMessage('manual-ack')
+  async handleManualAck(
+    @MessageBody() data: any,
+    @Ack() ack: (response: any) => void,
+  ) {
+    await new Promise(resolve => setTimeout(resolve, 20));
+
+    if (data.shouldSucceed) {
+      ack({ status: 'success', data });
+    } else {
+      ack({ status: 'error', message: 'Operation failed' });
+    }
+    return { status: 'ignored' };
   }
 }
