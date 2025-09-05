df7b5263b754001e20ae3b17e8a0dc467bb65a1e
ghy <ghyghoo8@qq.com>
2025-09-05 11:28:59 +0800

    Update invalid-tcp-message.exception.ts
    typo error message

--- packages/microservices/errors/invalid-tcp-message.exception.ts
+++ packages/microservices/errors/invalid-tcp-message.exception.ts
@@ -1,7 +1,8 @@
 import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.exception';
 
 export class InvalidTcpMessageException extends RuntimeException {
-  constructor() {
-    super(`The invalid data or message from tcp recv message.`);
+  constructor(err:string) {
+    const _errMsg = err.includes('Corrupted length value') ? 'Corrupted length value of the received data supplied in a packet.' : `The invalid received message from tcp server.`
+    super(_errMsg);
   }
 }
