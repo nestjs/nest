7d42cc9d16944f4de7ab09409b267e5a52674d96
ghy <ghyghoo8@qq.com>
2025-09-05 11:40:07 +0800

    Update and rename invalid-tcp-message.exception.ts to invalid-tcp-data-reception.exception.ts
    rename InvalidTcpDataReceptionException

--- packages/microservices/errors/invalid-tcp-data-reception.exception.ts
+++ packages/microservices/errors/invalid-tcp-data-reception.exception.ts
@@ -1,6 +1,6 @@
 import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.exception';
 
-export class InvalidTcpMessageException extends RuntimeException {
+export class InvalidTcpDataReceptionException extends RuntimeException {
   constructor(err:string) {
     const _errMsg = err.includes('Corrupted length value') ? 'Corrupted length value of the received data supplied in a packet.' : `The invalid received message from tcp server.`
     super(_errMsg);
