0bfd43cdd745b457fc363ace16cf23f3435e065d
ghy <ghyghoo8@qq.com>
2025-09-05 14:09:36 +0800

    fix: build error

--- packages/microservices/errors/invalid-tcp-data-reception.exception.ts
+++ packages/microservices/errors/invalid-tcp-data-reception.exception.ts
@@ -1,7 +1,7 @@
 import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.exception';
 
 export class InvalidTcpDataReceptionException extends RuntimeException {
-  constructor(name: string) {
+  constructor(err: string) {
     const _errMsg = err.includes('Corrupted length value') ? 'Corrupted length value of the received data supplied in a packet.' : 'The invalid received message from tcp server.';
     super(_errMsg);
   }
