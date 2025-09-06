9403addd4413eca8b524a2b8e730914958b141df
ghy <ghyghoo8@qq.com>
2025-09-06 16:15:52 +0800

    fix:lint
    lint fix at local

--- packages/microservices/errors/invalid-tcp-data-reception.exception.ts
+++ packages/microservices/errors/invalid-tcp-data-reception.exception.ts
@@ -2,8 +2,8 @@ import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.excepti
 
 export class InvalidTcpDataReceptionException extends RuntimeException {
   constructor(err: string) {
-    const _errMsg = err.includes('Corrupted length value') 
-      ? `Corrupted length value of the received data supplied in a packet` 
+    const _errMsg = err.includes('Corrupted length value')
+      ? `Corrupted length value of the received data supplied in a packet`
       : `The invalid received message from tcp server`;
     super(_errMsg);
   }
