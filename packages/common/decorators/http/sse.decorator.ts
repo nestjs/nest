fcfea5ed2af9407e06990a521f2fcbd35dcf21b4
Tomer Steinberg <tomerst77@gmail.com>
2025-10-31 17:31:02 +0200

    feat(common): add method options to @sse decorator

--- packages/common/decorators/http/sse.decorator.ts
+++ packages/common/decorators/http/sse.decorator.ts
@@ -6,7 +6,12 @@ import { RequestMethod } from '../../enums/request-method.enum';
  *
  * @publicApi
  */
-export function Sse(path?: string): MethodDecorator {
+export function Sse(
+  path?: string,
+  options: { [METHOD_METADATA]?: RequestMethod } = {
+    [METHOD_METADATA]: RequestMethod.GET,
+  },
+): MethodDecorator {
   return (
     target: object,
     key: string | symbol,
@@ -17,7 +22,7 @@ export function Sse(path?: string): MethodDecorator {
     Reflect.defineMetadata(PATH_METADATA, path, descriptor.value);
     Reflect.defineMetadata(
       METHOD_METADATA,
-      RequestMethod.GET,
+      options[METHOD_METADATA],
       descriptor.value,
     );
     Reflect.defineMetadata(SSE_METADATA, true, descriptor.value);
