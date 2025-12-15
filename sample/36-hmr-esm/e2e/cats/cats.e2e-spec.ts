6a2636925da3093dceb1c1cbf61a0cd04577ac12
shash-hq <shashankranjan.yt@gmail.com>
2025-12-15 10:29:58 +0530

    feat(sample): add hmr with esm example #14331

---
+++ sample/36-hmr-esm/e2e/cats/cats.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { CatsModule } from '../../src/cats/cats.module';
+import { CatsService } from '../../src/cats/cats.service';
+import { CoreModule } from '../../src/core/core.module';
+
+describe('Cats', () => {
+  const catsService = { findAll: () => ['test'] };
+
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CatsModule, CoreModule],
+    })
+      .overrideProvider(CatsService)
+      .useValue(catsService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`/GET cats`, () => {
+    return request(app.getHttpServer()).get('/cats').expect(200).expect({
+      data: catsService.findAll(),
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
---
+++ sample/36-hmr-esm/src/cats/cats.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './interfaces/cat.interface';
+
+@UseGuards(RolesGuard)
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  @Roles(['admin'])
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', new ParseIntPipe())
+    id: number,
+  ) {
+    // Retrieve a Cat instance by ID
+    console.log(id);
+  }
+}
