26e6062ab3fe848c9f851bb2aa1b8b75e493f06e
Muhammad Shareyar <shareyarz40@gmail.com>
2025-12-16 13:43:49 +0100

    Add comment for PR semgrep testing
    Added a comment to indicate the purpose of the hook.

--- hooks/mocha-init-hook.ts
+++ hooks/mocha-init-hook.ts
@@ -1,4 +1,5 @@
 export const mochaHooks = (): Mocha.RootHookObject => {
+  // Testing the PR semgrep testing.
   return {
     async beforeAll(this: Mocha.Context) {
       await import('reflect-metadata');
