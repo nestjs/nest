ec95499d4c844f571f8e8129b741a20cf99bc49c
Kamil Mysliwiec <mail@kamilmysliwiec.com>
2026-02-16 11:13:24 +0100

    Apply suggestion from @kamilmysliwiec

--- packages/common/interfaces/features/pipe-transform.interface.ts
+++ packages/common/interfaces/features/pipe-transform.interface.ts
@@ -19,7 +19,7 @@ export interface ArgumentMetadata<Metatype = any> {
    * Underlying base type (e.g., `String`) of the parameter, based on the type
    * definition in the route handler.
    */
-  readonly metatype?: Type<MetaType> | undefined;
+  readonly metatype?: Type<Metatype> | undefined;
   /**
    * String passed as an argument to the decorator.
    * Example: `@Body('userId')` would yield `userId`
