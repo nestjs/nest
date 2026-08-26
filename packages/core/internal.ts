c9d59f24d44b8d2dc4c602fe6dba47b22b991742
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2026-08-26 13:15:22 +0200

    chore: expose missing internals

--- packages/core/internal.ts
+++ packages/core/internal.ts
@@ -7,8 +7,9 @@
  */
 
 // Errors
-export { RuntimeException } from './errors/exceptions/runtime.exception.js';
+export * from './errors/exceptions/index.js';
 export { InvalidExceptionFilterException } from './errors/exceptions/invalid-exception-filter.exception.js';
+export { RuntimeException } from './errors/exceptions/runtime.exception.js';
 
 // Constants
 export { MESSAGES } from './constants.js';
@@ -17,33 +18,35 @@ export { MESSAGES } from './constants.js';
 export { DependenciesScanner } from './scanner.js';
 
 // Injector
+export { STATIC_CONTEXT } from './injector/constants.js';
 export { Injector, InjectorDependencyContext } from './injector/injector.js';
 export { InstanceLoader } from './injector/instance-loader.js';
 export { InstanceWrapper } from './injector/instance-wrapper.js';
+export { InternalCoreModule } from './injector/internal-core-module/index.js';
 export { Module } from './injector/module.js';
-export { STATIC_CONTEXT } from './injector/constants.js';
+export * from './inspector/index.js';
 
 // Helpers
-export { ExecutionContextHost } from './helpers/execution-context-host.js';
 export { ContextUtils, ParamProperties } from './helpers/context-utils.js';
+export { ExecutionContextHost } from './helpers/execution-context-host.js';
 export { HandlerMetadataStorage } from './helpers/handler-metadata-storage.js';
-export { RouterMethodFactory } from './helpers/router-method-factory.js';
 export { loadAdapter } from './helpers/load-adapter.js';
 export { optionalRequire } from './helpers/optional-require.js';
+export { RouterMethodFactory } from './helpers/router-method-factory.js';
 export { makeSafeInstanceDecorator } from './helpers/safe-instance-decorator.js';
 
 // Helpers - interfaces
 export { ParamsMetadata } from './helpers/interfaces/index.js';
 
 // Guards
+export { FORBIDDEN_MESSAGE } from './guards/constants.js';
 export { GuardsConsumer } from './guards/guards-consumer.js';
 export { GuardsContextCreator } from './guards/guards-context-creator.js';
-export { FORBIDDEN_MESSAGE } from './guards/constants.js';
 
 // Pipes
+export { ParamsTokenFactory } from './pipes/params-token-factory.js';
 export { PipesConsumer } from './pipes/pipes-consumer.js';
 export { PipesContextCreator } from './pipes/pipes-context-creator.js';
-export { ParamsTokenFactory } from './pipes/params-token-factory.js';
 
 // Interceptors
 export { InterceptorsConsumer } from './interceptors/interceptors-consumer.js';
