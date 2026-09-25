098c1a382879525d0192f1e7f7beae9352bb862c
Mike Drakoulelis <mikedrakoulelis@yahoo.gr>
2026-09-25 15:31:45 +0300

    chore: add ClassSerializerInterceptorOptions to public api

--- packages/common/serializer/class-serializer.interceptor.ts
+++ packages/common/serializer/class-serializer.interceptor.ts
@@ -28,6 +28,9 @@ export class ClassSerializerInterceptorOptions {
   transformerPackage?: TransformerPackage;
 }
 
+/**
+ * @publicApi
+ */
 export interface ClassSerializerInterceptorOptions extends ClassTransformOptions {
   transformerPackage?: TransformerPackage;
 }
